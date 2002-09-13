7c638b623d12c86c189cfee4d65b599d88d1d0e2
zimmerma <zimmerma@404564d9-a503-0410-82bf-e18ce2cf3989>
2002-09-13 13:28:08 +0000

    Initial revision

---
+++ ecm.c
@@ -0,0 +1,45 @@
+/* Elliptic Curve Method implementation.
+
+  Copyright (C) 2001 Paul Zimmermann,
+  LORIA/INRIA Lorraine, zimmerma@loria.fr
+  See http://www.loria.fr/~zimmerma/records/ecmnet.html
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the
+  Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; see the file COPYING.  If not, write to the Free
+  Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+  02111-1307, USA.
+*/
+
+#include "gmp.h"
+#include "ecm.h"
+
+/******************************************************************************
+*                                                                             *
+*                            Elliptic Curve Method                            *
+*                                                                             *
+******************************************************************************/
+
+/* Input: p is the initial seed (sigma)
+          n is the number to factor
+	  B1 is the stage 1 bound
+	  B2 is the stage 2 bound
+          k is the number of blocks for stage 2
+   Output: p is the factor found
+   Return value: non-zero iff a factor is found
+*/
+int
+ecm (mpz_t p, mpz_t n, double B1, double B2, unsigned int k)
+{
+  return 0;
+}
+
