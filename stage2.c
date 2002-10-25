49c374d9399f6ff17a1cd5af3c3358503d5f0f9a
zimmerma <zimmerma@404564d9-a503-0410-82bf-e18ce2cf3989>
2002-10-25 07:53:56 +0000

    added verbose flag

--- stage2.c
+++ stage2.c
@@ -28,13 +28,13 @@
 #include "ecm.h"
 #include "cputime.h"
 
-/* puts in F the successive values of s^j, for 0 < j < d, prime j
+/* puts in F the successive values of s^j, for 0 < j < d, j=1 mod 7, prime j.
    Return the degree of F, or 0 if a factor was found.
    Output: s is s0^6, invs = s0^(-6).
 */
 int
 rootsF (mpz_t *F, unsigned int d, mpz_t s, mpz_t invs, mpz_t t, mpz_t u,
-	mpz_t n)
+	mpz_t n, int verbose)
 {
   unsigned int i, j;
   int st;
@@ -48,7 +48,7 @@ rootsF (mpz_t *F, unsigned int d, mpz_t s, mpz_t invs, mpz_t t, mpz_t u,
       mpz_set (s, t);
       return 0;
     }
-  
+
   mpz_set (t, s); /* t = s0 */
   mpz_set (u, invs); /* u = 1/s0 */
 
@@ -72,35 +72,38 @@ rootsF (mpz_t *F, unsigned int d, mpz_t s, mpz_t invs, mpz_t t, mpz_t u,
 	}
     }
 
-  fprintf (stderr, "Computing roots of F took %dms\n", cputime () - st);
+  if (verbose >= 2)
+    printf ("Computing roots of F took %dms\n", cputime () - st);
 
   return i;
 }
 
-/* puts in G the successive values of s^j, for 1 <= j <= d
+/* puts in G the successive values of t0*s^j, for 1 <= j <= d
+   returns in t the value of t0*s^d, in u the value of u0*invs^d
 */
 void
 rootsG (mpz_t *G, unsigned int d, mpz_t s, mpz_t invs, mpz_t t, mpz_t u,
-	mpz_t n)
+	mpz_t n, int verbose)
 {
   unsigned int i;
   int st;
 
   st = cputime ();
 
-  for (i = 0; i < d; i++)
+  for (i = 1; i <= d; i++)
     {
       mpz_mul (t, t, s);
       mpz_mod (t, t, n);
       mpz_mul (u, u, invs);
       mpz_mod (u, u, n);
 
-      /* now t = s^i, u = s^(-i) */
+      /* now t = t0*s^i, u = u0*s^(-i) */
       mpz_add (G[i], t, u);
       mpz_mod (G[i], G[i], n);
     }
 
-  fprintf (stderr, "Computing roots of G took %dms\n", cputime () - st);
+  if (verbose >= 2)
+    printf ("Computing roots of G took %dms\n", cputime () - st);
 }
 
 /* Input:  x is the value at end of stage 1
@@ -111,16 +114,23 @@ rootsG (mpz_t *G, unsigned int d, mpz_t s, mpz_t invs, mpz_t t, mpz_t u,
    Return value: non-zero iff a factor was found.
 */
 int
-stage2 (mpz_t x, mpz_t n, double B2, unsigned int k)
+stage2 (mpz_t x, mpz_t n, double B2, unsigned int k, int verbose)
 {
   double b2;
-  unsigned int d, dF, dG;
-  mpz_t *F, *G, *T, invx, t, u;
-  int i, youpi = 0, st;
+  unsigned int i, d, dF, dG;
+  listz_t F, G, T;
+  polyz_t polyF, polyT;
+  mpz_t invx, t, u;
+  int youpi = 0, st, st0;
 
-  printf ("starting stage 2 with x=");
-  mpz_out_str (stdout, 10, x);
-  putchar ('\n');
+  st0 = cputime ();
+
+  if (verbose >= 2)
+    {
+      printf ("starting stage 2 with x=");
+      mpz_out_str (stdout, 10, x);
+      putchar ('\n');
+    }
 
   b2 = ceil(B2 / k); /* b2 = ceil(B2/k): small block size */
 
@@ -133,73 +143,83 @@ stage2 (mpz_t x, mpz_t n, double B2, unsigned int k)
   dF = phi (d) / 2;
   dG = dF - 1;
 
-  printf ("B2=%1.0f b2=%1.0f d=%u dF=%u dG=%u\n", B2, b2, d, dF, dG);
+  if (verbose >= 2)
+    printf ("B2=%1.0f b2=%1.0f d=%u dF=%u dG=%u\n", B2, b2, d, dF, dG);
 
-  F = init_poly (dF + 1); 
+  F = init_list (dF + 1); 
 
   mpz_init (invx);
   mpz_init (t);
   mpz_init (u);
-  if ((i = rootsF (F, d, x, invx, t, u, n)) == 0)
+  if ((i = rootsF (F, d, x, invx, t, u, n, verbose)) == 0)
     {
-      youpi = 1;
+      youpi = 2;
       goto clear_F;
     }
 
   assert (i == dF);
   
-  T = init_poly (5 * dF - 1);
-  buildG (F, dF, T, 1, n, 'F');
+  T = init_list (5 * dF - 1);
+  buildG (F, dF, T, verbose, n, 'F');
 
   mpz_powm_ui (x, x, d / 6, n);
   mpz_powm_ui (invx, invx, d / 6, n);
 
   /* now x = x0^d, and invx = x0^(-d) */
 
-  G = init_poly (dF);
+  G = init_list (dF);
 
   mpz_set_ui (t, 1);
   mpz_set_ui (u, 1);
 
   for (i=0; i<k; i++)
     {
-      rootsG (G, dG, x, invx, t, u, n);
+      rootsG (G, dG, x, invx, t, u, n, verbose);
 
       buildG (G, dG, T + dF, 1, n, 'G');
       mpz_set_ui (G[dG], 1);
 
       if (i == 0)
-	  polyset (T, G, dF);
+	  list_set (T, G, dF);
       else
 	{
 	  st = cputime ();
 	  /* previous G is in T, with degree < dF, i.e. dF coefficients
 	     and dG = dF - 1 */
-	  polymulmod (T + dF, G, T, dF, T + 3 * dF, n);
-	  fprintf (stderr, "Computing G * H took %dms\n", cputime() - st);
+	  list_mulmod (T + dF, G, T, dF, T + 3 * dF, n);
+          if (verbose >= 2)
+            printf ("Computing G * H took %dms\n", cputime() - st);
 	  st = cputime ();
           mpz_set_ui (T[3*dF-1], 0); /* since RecursiveDivision expects a
                                         dividend of 2*dF coefficients */
 	  RecursiveDivision (T, T + dF, F, dF, T + 3 * dF, n);
-          polyset (T, T + dF, dF);
-	  fprintf (stderr, "Reducing G * H mod F took %dms\n", cputime() - st);
+          list_set (T, T + dF, dF);
+          if (verbose >= 2)
+            printf ("Reducing G * H mod F took %dms\n", cputime() - st);
 	}
     }
 
   mpz_set_ui (F[dF], 1); /* the leading monic coefficient needs to be stored
                              explicitely for polygcd */
   st = cputime ();
-  youpi = polygcd (x, F, T, dF, n, T + dF);
-  fprintf (stderr, "Computing gcd of F and G took %dms\n", cputime() - st);
+  init_poly_list (polyF, dF, F);
+  init_poly_list (polyT, dF - 1, T);
+  if ((youpi = poly_gcd (x, polyF, polyT, n, T + dF)))
+    NTL_get_factor (x);
+  if (verbose >= 2)
+    printf ("Computing gcd of F and G took %dms\n", cputime() - st);
 
-  clear_poly (G, dG);
-  clear_poly (T, 2 * dF);
+  clear_list (G, dF);
+  clear_list (T, 5 * dF - 1);
 
  clear_F:
   mpz_clear (invx);
   mpz_clear (t);
   mpz_clear (u);
-  clear_poly (F, dF);
+  clear_list (F, dF + 1);
+
+  if (verbose >= 1)
+    printf ("Stage 2 took %dms\n", cputime() - st0);
 
   return youpi;
 }
