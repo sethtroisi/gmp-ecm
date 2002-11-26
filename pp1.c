c75672e0260426224c9b435e1015dbfe9dd4dddc
zimmerma <zimmerma@404564d9-a503-0410-82bf-e18ce2cf3989>
2002-11-26 16:05:24 +0000

    Williams P+1 method

---
+++ pp1.c
@@ -0,0 +1,248 @@
+/* Williams 'P+1' algorithm.
+
+  Copyright (C) 2002 Alexander Kruppa and Paul Zimmermann.
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the
+  Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; see the file COPYING.  If not, write to the Free
+  Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+  02111-1307, USA.
+
+  References:
+
+  A p+1 Method of Factoring, H. C. Williams, Mathematics of Computation,
+  volume 39, number 159, pages 225-234, 1982.
+
+  Evaluating recurrences of form X_{m+n} = f(X_m, X_n, X_{m-n}) via
+  Lucas chains, Peter L. Montgomery, December 1983, revised January 1992.
+*/
+
+#include <math.h>
+#include <stdio.h>
+#include "gmp.h"
+#include "gmp-impl.h"
+#include "longlong.h"
+#include "ecm.h"
+
+/******************************************************************************
+*                                                                             *
+*                                  Stage 1                                    *
+*                                                                             *
+******************************************************************************/
+
+/* prime powers are accumulated up to about n^L1 */
+#define L1 1
+
+/* P1 <- V_e(P0), using P, Q as auxiliary variables,
+   where V_{2k}(P0) = V_k(P0)^2 - 2
+         V_{2k-1}(P0) = V_k(P0)*V_{k-1}(P0) - P0
+*/
+void
+pp1_mul (mpz_t P1, mpz_t P0, mpz_t P, mpz_t Q, mpz_t e, mpz_t n)
+{
+  unsigned long i;
+
+  if (mpz_cmp_ui (e, 0) == 0)
+    {
+      mpz_set_ui (P1, 2);
+      return;
+    }
+
+  mpz_sub_ui (e, e, 1);
+  mpz_set (P, P0);
+  mpz_set_ui (Q, 2);
+  /* invariant: (P, Q) = (V_{k+1}(P0), V_k(P0)), start with k=0 */
+  for (i = mpz_sizeinbase (e, 2); i > 0;)
+    {
+      if (mpz_tstbit (e, --i)) /* k -> 2k+1 */
+        {
+          if (i) /* Q is not needed for last iteration */
+            {
+              mpz_mul (Q, P, Q);
+              mpz_sub (Q, Q, P0);
+              mpz_mod (Q, Q, n);
+            }
+          mpz_mul (P, P, P);
+          mpz_sub_ui (P, P, 2);
+        }
+      else /* k -> 2k */
+        {
+          mpz_mul (P, P, Q);
+          mpz_sub (P, P, P0);
+          if (i) /* Q is not needed for last iteration */
+            {
+              mpz_mul (Q, Q, Q);
+              mpz_sub_ui (Q, Q, 2);
+              mpz_mod (Q, Q, n);
+            }
+        }
+      if (i)
+        mpz_mod (P, P, n);
+    }
+
+  mpz_mod (P1, P, n);
+}
+
+/* P1 <- e * P0, using P, Q as auxiliary variables */
+void
+pp1_mul_ui (mpz_t P1, mpz_t P0, mpz_t P, mpz_t Q, mp_limb_t e, mpz_t n)
+{
+  unsigned long i;
+  mp_limb_t mask;
+
+  e = e - 1;
+  count_leading_zeros (i, e);
+  i = BITS_PER_MP_LIMB - i;
+  mask = (mp_limb_t) 1 << (i - 1);
+  mpz_set (P, P0);
+  mpz_set_ui (Q, 2);
+  for (; i > 0; i--)
+    {
+      if (e & mask)
+        {
+          if (i) /* Q is not needed for last iteration */
+            {
+              mpz_mul (Q, P, Q);
+              mpz_sub (Q, Q, P0);
+              mpz_mod (Q, Q, n);
+            }
+          mpz_mul (P, P, P);
+          mpz_sub_ui (P, P, 2);
+        }
+      else
+        {
+          mpz_mul (P, P, Q);
+          mpz_sub (P, P, P0);
+          if (i) /* Q is not needed for last iteration */
+            {
+              mpz_mul (Q, Q, Q);
+              mpz_sub_ui (Q, Q, 2);
+              mpz_mod (Q, Q, n);
+            }
+        }
+      if (i)
+        mpz_mod (P, P, n);
+      mask >>= 1;
+    }
+
+  mpz_mod (P1, P, n);
+}
+
+/* Input:  P0 is the initial point (sigma)
+           n is the number to factor
+           B1 is the stage 1 bound
+   Output: a is the factor found, or the value at end of stage 1
+   Return value: non-zero iff a factor was found.
+*/
+int
+pp1_stage1 (mpz_t P0, mpz_t n, double B1)
+{
+  double B0, p, q, r;
+  mpz_t P, Q, g;
+  int youpi;
+  unsigned int max_size;
+
+  mpz_init (P);
+  mpz_init (Q);
+  mpz_init (g);
+
+  B0 = sqrt (B1);
+
+  max_size = L1 * mpz_sizeinbase (n, 2);
+
+  /* suggestion from Peter Montgomery: start with exponent n-1,
+     since any prime divisor of b^m-1 which does not divide any
+     algebraic factor of b^m-1 must be of the form km+1 [Williams82].
+     Do this only when n is composite, otherwise all tests with prime
+     n factor of a Cunningham number will succeed in stage 1. */
+  if (mpz_probab_prime_p (n, 1) == 0)
+    {
+      mpz_sub_ui (g, n, 1);
+      pp1_mul (P0, P0, P, Q, g, n);
+    }
+  else
+    mpz_set_ui (g, 1);
+
+  /* first loop through small primes <= sqrt(B1) */
+  for (p = 2.0; p <= B0; p = getprime(p))
+    {
+      for (q = 1, r = p; r <= B1; q = r, r *= p);
+      mpz_mul_d (g, g, q, Q);
+      if (mpz_sizeinbase (g, 2) >= max_size)
+	{
+	  pp1_mul (P0, P0, P, Q, g, n);
+	  mpz_set_ui (g, 1);
+	}
+    }
+
+  /* then all primes > sqrt(B1) and taken with exponent 1 */
+  for (; p <= B1; p = getprime(p))
+    {
+      mpz_mul_d (g, g, p, Q);
+      if (mpz_sizeinbase (g, 2) >= max_size)
+	{
+	  pp1_mul (P0, P0, P, Q, g, n);
+	  mpz_set_ui (g, 1);
+	}
+    }
+
+  getprime (0.0); /* free the prime tables, and reinitialize */
+
+  pp1_mul (P0, P0, P, Q, g, n);
+
+  mpz_clear (P);
+  mpz_clear (Q);
+
+  mpz_sub_ui (g, P0, 2);
+  mpz_gcd (g, g, n);
+  if ((youpi = mpz_cmp_ui (g, 1)))
+    mpz_set (P0, g);
+
+  mpz_clear (g);
+  
+  return youpi;
+}
+
+/******************************************************************************
+*                                                                             *
+*                               Williams P+1                                  *
+*                                                                             *
+******************************************************************************/
+
+/* Input: p is the initial generator (sigma)
+          n is the number to factor
+	  B1 is the stage 1 bound
+	  B2 is the stage 2 bound
+          k is the number of blocks for stage 2
+          verbose is the verbose level: 0=quiet, 1=normal, 2=verbose
+   Output: p is the factor found
+   Return value: non-zero iff a factor is found (1 for stage 1, 2 for stage 2)
+*/
+int
+pp1 (mpz_t p, mpz_t n, double B1, double B2, unsigned int k, unsigned int S, 
+     int verbose)
+{
+  int youpi, st;
+
+  st = cputime ();
+  youpi = pp1_stage1 (p, n, B1);
+  if (verbose >= 1)
+    {
+      printf ("Stage 1 took %dms\n", cputime() - st);
+      fflush (stdout);
+    }
+
+  if (youpi != 0) /* a factor was found */
+    return 1;
+
+  return (B2 > B1) ? stage2 (p, n, B2, k, S, verbose, 0, PP1_METHOD) : 0;
+}
