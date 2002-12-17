168a56f478e08da7c47b91302c453f1ae641c124
zimmerma <zimmerma@404564d9-a503-0410-82bf-e18ce2cf3989>
2002-12-17 17:59:48 +0000

    started to adapt to generic modular multiplication

--- lucas.c
+++ lucas.c
@@ -29,32 +29,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "gmp.h"
+#include "ecm.h"
 
-#define ADD 1
-#define DUP 1
+unsigned int lucas_cost_pp1 (unsigned, double);
+
+/* the following constant were tested experimentally: in theory, we should
+   have ADD = 2*DUP in the basecase range, and ADD = 3/2*DUP in the FFT range.
+   Only the ratio ADD/DUP counts. */
+#define ADD 3 /* cost of add3:      one modular multiply */
+#define DUP 2 /* cost of duplicate: one square */
 
 #define duplicate pp1_duplicate
 #define add3      pp1_add3
 
 /* P <- V_2(Q) */
-inline void
-duplicate (mpz_t P, mpz_t Q, mpz_t n)
+static void
+duplicate (mpres_t P, mpres_t Q, mpmod_t n)
 {
-  mpz_mul (P, Q, Q);
-  mpz_sub_ui (P, P, 2);
-  mpz_mod (P, P, n);
+  mpres_mul (P, Q, Q, n);
+  mpres_sub_ui (P, P, 2, n);
+  mpmod_mod (P, P, n);
 }
 
 /* P <- V_{m+n} where Q = V_m, R = V_n, S = V_{m-n}.
    t is an auxiliary variable.
    Warning: P may equal Q, R or S.
 */
-inline void
-add3 (mpz_t P, mpz_t Q, mpz_t R, mpz_t S, mpz_t n, mpz_t t)
+static void
+add3 (mpres_t P, mpres_t Q, mpres_t R, mpres_t S, mpmod_t n, mpres_t t)
 {
-  mpz_mul (t, Q, R);
-  mpz_sub (t, t, S);
-  mpz_mod (P, t, n);
+  mpres_mul (t, Q, R, n);
+  mpres_sub (t, t, S, n);
+  mpmod_mod (P, t, n);
 }
 
 /* returns the number of modular multiplications for computing
@@ -141,20 +147,21 @@ lucas_cost_pp1 (unsigned n, double v)
 
 #define NV 4
 
-// #define SWAP(x,y) { __mpz_struct *tmp = x; x = y; y = tmp; }
-#define SWAP mpz_swap
+/* #define SWAP(x,y) { __mpz_struct *tmp = x; x = y; y = tmp; } */
+#define SWAP mpres_swap
 
 /* computes V_k(P) from P=A and puts the result in P=A. Assumes k>2.
    Uses auxiliary variables t, B, C, T, T2.
 */
-void
-pp1_mul_prac (mpz_t A, unsigned long k, mpz_t n, mpz_t t, mpz_t B, mpz_t C,
-          mpz_t T, mpz_t T2)
+int
+pp1_mul_prac (mpres_t A, unsigned long k, mpmod_t n, mpres_t t, mpres_t B,
+              mpres_t C, mpres_t T, mpres_t T2)
 {
   unsigned int d, e, r, i = 0;
   static double val[NV] =
-  { 1.61803398875, 1.72360679775, 1.618347119656, 1.617914406529};
-  
+  {1.61803398875, 1.72360679775, 1.618347119656, 1.617914406529};
+  unsigned long muls;
+
   /* chooses the best value of v */
   for (d = 0, r = ADD * k; d < NV; d++)
     {
@@ -171,9 +178,10 @@ pp1_mul_prac (mpz_t A, unsigned long k, mpz_t n, mpz_t t, mpz_t B, mpz_t C,
   /* first iteration always begins by Condition 3, then a swap */
   d = k - r;
   e = 2 * r - k;
-  mpz_set (B, A); /* B=A */
-  mpz_set (C, A); /* C=A */
+  mpres_set (B, A, n); /* B=A */
+  mpres_set (C, A, n); /* C=A */
   duplicate (A, A, n); /* A = 2*A */
+  muls = 1;
   while (d != e)
     {
       if (d < e)
@@ -181,7 +189,7 @@ pp1_mul_prac (mpz_t A, unsigned long k, mpz_t n, mpz_t t, mpz_t B, mpz_t C,
           r = d;
           d = e;
           e = r;
-          mpz_swap (A, B);
+          mpres_swap (A, B, n);
         }
       /* do the first line of Table 4 whose condition qualifies */
       if (4 * d <= 5 * e && ((d + e) % 3) == 0)
@@ -191,31 +199,36 @@ pp1_mul_prac (mpz_t A, unsigned long k, mpz_t n, mpz_t t, mpz_t B, mpz_t C,
           add3 (T,  A, B, C, n, t); /* T = f(A,B,C) */
           add3 (T2, T, A, B, n, t); /* T2 = f(T,A,B) */
           add3 (B,  B, T, A, n, t); /* B = f(B,T,A) */
-          mpz_swap (A, T2);         /* swap A and T2 */
+          mpres_swap (A, T2, n);    /* swap A and T2 */
+          muls += 3;
         }
       else if (4 * d <= 5 * e && (d - e) % 6 == 0)
         { /* condition 2 */
           d = (d - e) / 2;
           add3 (B, A, B, C, n, t); /* B = f(A,B,C) */
           duplicate (A, A, n);     /* A = 2*A */
+          muls += 2;
         }
       else if (d <= (4 * e))
         { /* condition 3 */
           d -= e;
           add3 (C, B, A, C, n, t); /* C = f(B,A,C) */
-          SWAP (B, C);
+          SWAP (B, C, n);
+          muls ++;
         }
       else if ((d + e) % 2 == 0)
         { /* condition 4 */
           d = (d - e) / 2;
           add3 (B, B, A, C, n, t); /* B = f(B,A,C) */
           duplicate (A, A, n);     /* A = 2*A */
+          muls += 2;
         }
       else if (d % 2 == 0)
         { /* condition 5 */
           d /= 2;
           add3 (C, C, A, B, n, t); /* C = f(C,A,B) */
           duplicate (A, A, n);     /* A = 2*A */
+          muls += 2;
         }
       else if (d % 3 == 0)
         { /* condition 6 */
@@ -224,7 +237,8 @@ pp1_mul_prac (mpz_t A, unsigned long k, mpz_t n, mpz_t t, mpz_t B, mpz_t C,
           add3 (T2, A, B, C, n, t);  /* T2 = f(A,B,C) */
           add3 (A,  T, A, A, n, t);  /* A = f(T,A,A) */
           add3 (C,  T, T2, C, n, t); /* C = f(T,T2,C) */
-          SWAP (B, C);
+          SWAP (B, C, n);
+          muls += 4;
         }
       else if ((d + e) % 3 == 0)
         { /* condition 7 */
@@ -233,21 +247,24 @@ pp1_mul_prac (mpz_t A, unsigned long k, mpz_t n, mpz_t t, mpz_t B, mpz_t C,
           add3 (B, T, A, B, n, t); /* B = f(T1,A,B) */
           duplicate (T, A, n);
           add3 (A, A, T, A, n, t); /* A = 3*A */
+          muls += 4;
         }
       else if ((d - e) % 3 == 0)
         { /* condition 8: never happens? */
           d = (d - e) / 3;
           add3 (T, A, B, C, n, t); /* T1 = f(A,B,C) */
           add3 (C, C, A, B, n, t); /* C = f(A,C,B) */
-          SWAP (B, T);         /* swap B and T */
+          SWAP (B, T, n);          /* swap B and T */
           duplicate (T, A, n);
           add3 (A, A, T, A, n, t); /* A = 3*A */
+          muls += 4;
         }
       else if (e % 2 == 0)
         { /* condition 9: never happens? */
           e /= 2;
           add3 (C, C, B, A, n, t); /* C = f(C,B,A) */
           duplicate (B, B, n);     /* B = 2*B */
+          muls += 2;
         }
       else
         {
@@ -257,10 +274,14 @@ pp1_mul_prac (mpz_t A, unsigned long k, mpz_t n, mpz_t t, mpz_t B, mpz_t C,
     }
   
   add3 (A, A, B, C, n, t);
-  
+
+#ifdef DEBUG  
   if (d != 1)
     {
-      fprintf (stderr, "d!=1 at the end of PRAC\n");
+      fprintf (stderr, "d!=1 at the end of PRAC: %u\n", d);
       exit (1);
     }
+#endif
+
+  return muls + 1;
 }
