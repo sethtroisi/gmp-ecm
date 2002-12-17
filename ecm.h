863c490e72ada6d0cd713377788b1731c21a3972
zimmerma <zimmerma@404564d9-a503-0410-82bf-e18ce2cf3989>
2002-12-17 17:57:54 +0000

    modified for ecm stage 2

--- ecm.h
+++ ecm.h
@@ -1,3 +1,20 @@
+#define ECM_VERSION "5.0"
+
+#define mpmod_t mpz_t
+#define mpres_t mpz_t
+#define mpmod_mod mpz_mod
+#define mpres_set(a,b,n) mpz_set(a,b)
+#define mpres_mul(a,b,c,n) mpz_mul(a,b,c)
+#define mpres_sub_ui(a,b,c,n) mpz_sub_ui(a,b,c)
+#define mpres_sub(a,b,c,n) mpz_sub(a,b,c)
+#define mpres_swap(a,b,n) mpz_swap(a,b)
+#define mpres_init(a,n) mpz_init(a)
+#define mpres_clear(a,n) mpz_clear(a)
+
+#define mpz_mulmod(a,b,c,n) \
+        { mpz_mul (a, b, c); \
+        mpz_mod (a, a, n); }
+
 /* maximal stage 1 bound = 2^53 + 4, the next prime being 2^53 + 5 */
 #define MAX_B1 9007199254740996.0
 
@@ -10,18 +27,36 @@ typedef mpz_t* listz_t;
 
 typedef struct
 {
+  mpz_t x;
+  mpz_t y;
+} __point_struct;
+typedef __point_struct point;
+
+typedef struct
+{
+  mpz_t x;
+  mpz_t y;
+  mpz_t A;
+} __curve_struct;
+typedef __curve_struct curve;
+
+typedef struct
+{
   int alloc;
   int degree;
   listz_t coeff;
 } __polyz_struct;
 typedef __polyz_struct polyz_t[1];
 
+#if defined (__cplusplus)
+extern "C" {
+#endif  
+
 double   getprime       (double);
 unsigned int nb_digits  (mpz_t);
 
 /* pm1.c */
 void    pm1_random_seed (mpz_t, mpz_t, gmp_randstate_t);
-int      pm1_stage1     (mpz_t, mpz_t, double, double);
 int          pm1        (mpz_t, mpz_t, mpz_t, double, double, double, 
                          unsigned int, unsigned int, int);
 
@@ -34,23 +69,22 @@ double       block_size (unsigned int);
 unsigned int gcd        (unsigned int, unsigned int);
 int          cputime    (void);
 
+/* ecm2.c */
+int multiplyW2 (mpz_t, mpz_t, mpz_t, mpz_t, mpz_t, mpz_t, mpz_t, mpz_t, mpz_t,
+                mpz_t);
+int addWn      (mpz_t, point *, mpz_t, long);
+
 /* pp1.c */
-void         pp1_mul    (mpz_t, mpz_t, mpz_t, mpz_t, mpz_t, mpz_t);
-void         pp1_mul_ui (mpz_t, mpz_t, unsigned long, mpz_t, mpz_t, mpz_t);
-void         pp1_mul_prac (mpz_t, unsigned long, mpz_t, mpz_t, mpz_t, mpz_t,
-                           mpz_t, mpz_t);
-int          pp1_stage1 (mpz_t, mpz_t, double, double);
+int          pp1_mul    (mpz_t, mpz_t, mpz_t, mpz_t, mpz_t, mpz_t);
+int          pp1_mul_prac(mpres_t, unsigned long, mpmod_t, mpres_t, mpres_t,
+                          mpres_t, mpres_t, mpres_t);
 void    pp1_random_seed (mpz_t, mpz_t, gmp_randstate_t);
 int          pp1        (mpz_t, mpz_t, mpz_t, double, double, double, 
                          unsigned int, unsigned int, int);
 
 /* stage2.c */
-int          rootsF     (listz_t, unsigned int, mpz_t, mpz_t, mpz_t *, 
-                         unsigned int , mpz_t, int, int);
-void         rootsG     (listz_t, unsigned int, listz_t, listz_t, 
-                         listz_t, unsigned int, mpz_t, int);
-int          stage2     (mpz_t, mpz_t, mpz_t, double, unsigned int, 
-                         unsigned int, int, int, int);
+int          stage2     (mpz_t, curve *, mpz_t, double, unsigned int, 
+                         unsigned int, int, int, double);
 
 /* listz.c */
 int          list_mul_mem (unsigned int);
@@ -104,6 +138,19 @@ int ntl_poly_gcd   (mpz_t, polyz_t, polyz_t, mpz_t);
 void NTL_init (void);
 void NTL_clear (void);
 void NTL_get_factor (mpz_t);
+
+void __gmp_default_free (void *, size_t);
+#ifdef MEMORY_DEBUG
+void *__gmp_default_allocate (size_t);
+void *__gmp_default_reallocate (void *, size_t, size_t);
+void tests_memory_start (void);
+void tests_memory_end   (void);
+void tests_memory_reset (void);
+#endif
+
+#if defined (__cplusplus)
+}
+#endif  
 
 /* a <- b * c where a and b are mpz, c is a double, and t an auxiliary mpz */
 #if (BITS_PER_MP_LIMB >= 53)
