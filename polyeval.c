1ef432b53b83179ee8dadb8ac9e8d2d23a429efc
zimmerma <zimmerma@404564d9-a503-0410-82bf-e18ce2cf3989>
2003-12-12 14:55:31 +0000

    changed copyright line

--- polyeval.c
+++ polyeval.c
@@ -1,6 +1,6 @@
 /* Implements algorithm POLYEVAL.
 
-  Copyright 2003 Paul Zimmermann and Alexander Kruppa.
+  Copyright 2003 Laurent Fousse, Alexander Kruppa, and Paul Zimmermann.
 
   This program is free software; you can redistribute it and/or modify it
   under the terms of the GNU General Public License as published by the
