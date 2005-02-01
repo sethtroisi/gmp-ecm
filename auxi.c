341d3df9296ab3e61f58d1c078c2508a177b6bdb
Paul Zimmerman <paul.zimmerman@inria.fr>
2005-02-01 10:46:44 +0000

    added missing years
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ecm/trunk@602 404564d9-a503-0410-82bf-e18ce2cf3989

--- auxi.c
+++ auxi.c
@@ -1,6 +1,6 @@
 /* Auxiliary functions for GMP-ECM.
 
-  Copyright 2001, 2002, 2003 Paul Zimmermann and Alexander Kruppa.
+  Copyright 2001, 2002, 2003, 2004, 2005 Paul Zimmermann and Alexander Kruppa.
 
   This program is free software; you can redistribute it and/or modify it
   under the terms of the GNU General Public License as published by the
