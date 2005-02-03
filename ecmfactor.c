eaaa111ce23c394820dbc98173c4c65a95e6535b
Paul Zimmerman <paul.zimmerman@inria.fr>
2005-02-03 14:04:32 +0000

    improved readability
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ecm/trunk@637 404564d9-a503-0410-82bf-e18ce2cf3989

--- ecmfactor.c
+++ ecmfactor.c
@@ -9,6 +9,9 @@ main (int argc, char *argv[])
 {
   mpz_t n, f, x, sigma;
   int res;
+  double B1 = 1e3; /* stage 1 bound */
+  double B2 = 1e5; /* stage 2 bound */
+  double B2min = B1; /* lower bound for stage 2 */
 
   if (argc != 2)
     {
@@ -32,7 +35,10 @@ main (int argc, char *argv[])
   /* set sigma at random */
   mpz_set_ui (sigma, getpid ());
 
-  res = ecm (f, x, sigma, n, NULL, 1.0, 1e3, 1e3, 1e5, 1.0, ECM_DEFAULT_K,
+  printf ("Performing one curve with B1=%1.0f, B2=%1.0f, sigma=%lu\n",
+          B1, B2, mpz_get_ui (sigma));
+
+  res = ecm (f, x, sigma, n, NULL, 1.0, B1, B2min, B2, 1.0, ECM_DEFAULT_K,
              1, 0, 0, 0);
 
   if (res)
@@ -40,11 +46,9 @@ main (int argc, char *argv[])
       printf ("found factor ");
       mpz_out_str (stdout, 10, f);
       printf ("\n");
-      printf ("lucky curve was g*y^2 = x^3 + a*x^2 + x\n");
-      printf ("with a = (v-u)^3*(3*u+v),");
-      printf (" u = sigma^2-5, v = 4*sigma, sigma = ");
-      mpz_dump (sigma);
-
+      printf ("lucky curve was b*y^2 = x^3 + a*x^2 + x\n");
+      printf ("with a = (v-u)^3*(3*u+v)/(4*u^3*v)-2,");
+      printf (" u = sigma^2-5, v = 4*sigma\n");
     }
   else
     printf ("found no factor\n");
