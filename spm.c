debf52d51a140921706ed7bf633bb02c69895687
dnewman <dnewman@404564d9-a503-0410-82bf-e18ce2cf3989>
2005-03-04 12:33:06 +0000

    added licence headers

--- spm.c
+++ spm.c
@@ -1,3 +1,24 @@
+/* spm.c - "small prime modulus" functions to precompute an inverse and a
+   primitive root for a small prime
+
+  Copyright 2005 Dave Newman.
+
+  The SP Library is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or (at your
+  option) any later version.
+
+  The SP Library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+  License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with the SP Library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+  MA 02111-1307, USA.
+*/
+
 #include <stdlib.h>
 #include "sp.h"
 
