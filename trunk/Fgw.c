4d6924b91e97f4727b5447515a774fba519dda47
kruppa <kruppa@404564d9-a503-0410-82bf-e18ce2cf3989>
2005-06-07 19:32:43 +0000

    Interface to Woltman's GWNUM stage 1 for ECM

--- trunk/Fgw.c
+++ trunk/Fgw.c
@@ -2,7 +2,7 @@
   Interface code for George Woltman's gwnum library
   
   Copyright 2004 Paul Zimmermann and Alexander Kruppa.
-
+  
   Contains code based on the GWNUM library, 
     copyright 2002-2005 George Woltman, Just For Fun Software, Inc.
   
@@ -32,10 +32,36 @@
 #include <stdio.h>
 #include <gmp.h>
 #include "ecm-gmp.h"
+#include "ecm.h"
 #include "ecm-impl.h"
 #define ADD_UNDERSCORES
+#include "gwdbldbl.h"
 #include "gwnum.h"
 #include "cpuid.h"
+
+#define USE_FCONV
+//#define DEBUG
+
+/* This prototype should be in gwnum.h */
+int gwnum_ecmStage1 (
+        double,                         /* K in K*B^N+C */
+        unsigned long,                  /* B in K*B^N+C */
+        unsigned long,                  /* N in K*B^N+C */
+        signed long,                    /* C in K*B^N+C */
+        unsigned long *,                /* Number to factor */
+        unsigned long,
+        unsigned long,                  /* Stage 1 bound */
+        unsigned long *,                /* Stage 1 that is already done */
+        unsigned long *,                /* A - caller derives it from sigma */
+        unsigned long,
+        unsigned long *,                /* X value of point */
+        unsigned long *,
+        unsigned long *,                /* Z value of point */
+        unsigned long *,
+        int     (*)(void),              /* Ptr to proc that returns TRUE */
+                                        /* if user interrupts processing */
+        unsigned long options);
+
 
 typedef struct {
   char *bigword;
@@ -54,18 +80,28 @@ extern int ZERO_PADDED_FFT;
 static gwnum g1 = NULL, g2 = NULL;
 static gwconvplan_t *gwplan = NULL;
 
+static int 
+sgn (int i)
+{
+  if (i == 0)
+    return 0;
+  return i > 0 ? 1 : -1;
+}
+
 static gwconvplan_t *
 make_conv_plan ()
 {
   gwconvplan_t *plan;
   unsigned int i;
   unsigned long offset, lastoff;
+  const unsigned int planlen = FFTLEN + 1; 
+  /* One extra or preloading values in loop runs off end of array */
 
   plan = (gwconvplan_t *) malloc (sizeof (gwconvplan_t));
-  plan->bigword = (char *) malloc (FFTLEN);
-  plan->weights = (double *) malloc (FFTLEN * sizeof (double));
-  plan->invweights = (double *) malloc (FFTLEN * sizeof (double));
-  plan->offsets = (unsigned long *) malloc (FFTLEN * sizeof (long));
+  plan->bigword = (char *) malloc (planlen);
+  plan->weights = (double *) malloc (planlen * sizeof (double));
+  plan->invweights = (double *) malloc (planlen * sizeof (double));
+  plan->offsets = (unsigned long *) malloc (planlen * sizeof (long));
 
   lastoff = 0;
 
@@ -78,10 +114,15 @@ make_conv_plan ()
       plan->invweights[i] = gwfft_weight_inverse (i);
       lastoff = offset;
     }
+  plan->bigword[i] = 0;
+  plan->offsets[i] = 0;
+  plan->weights[i] = 0.;
+  plan->invweights[i] = 0.;
 
   return plan;
 }
 
+#ifdef DEBUG
 static void 
 gwdump (gwnum g)
 {
@@ -93,6 +134,7 @@ gwdump (gwnum g)
     }
   printf ("\n");
 }
+#endif
 
 /* Convert a number stored as an array of dwords to the gwnum FFT format. */
 /* Assumes 16 bits per FFT element and all DWT weights == 1.0 */
@@ -367,7 +409,7 @@ Fgwclear ()
 void
 Fgwmul (mpz_t R, mpz_t S1, mpz_t S2)
 {
-  int sgnchg = 0;
+  int sgnS1, sgnS2;
 #ifdef DEBUG
   mpz_t t;
 #endif
@@ -378,55 +420,217 @@ Fgwmul (mpz_t R, mpz_t S1, mpz_t S2)
       exit (EXIT_FAILURE);
     }
 
+  /* Make both input arguments nonnegative, remember original sign */
+  sgnS1 = mpz_sgn (S1);
+  sgnS2 = mpz_sgn (S2);
+  if (sgnS1 < 0)
+    mpz_neg (S1, S1);
+  /* Warning: S1 == S2 is possible! */
+  if (sgnS2 < 0 && S1 != S2)
+    mpz_neg (S2, S2);
+
 #ifdef DEBUG
   /* Test if converting to gwnum and back is identity */
   mpz_init (t);
   _mpz_realloc (t, FFTLEN / 2);
+
+  /* Test for S1 */
+#ifdef USE_FCONV
   Fdwordstogw (PTR(S1), ABSIZ(S1), g1, gwplan);
   Fgwtodwords (g1, PTR(t), &SIZ(t), ALLOC(t), gwplan);
-  if (mpz_sgn (S1) < 0)
-    mpz_neg (t, t);
+#else
+  binarytogw (PTR(S1), ABSIZ(S1), g1);
+  SIZ(t) = gwtobinary (g1, PTR(t), ALLOC(t));
+#endif
   if (mpz_cmp (S1, t) != 0)
     {
-      gmp_printf ("Fdwordstogw/Fgwtodwords is not identity for %Zd\nResult: %Zd\ng = ", 
-                  S1, t);
+      gmp_printf (
+#ifdef USE_FCONV
+                  "Fdwordstogw/Fgwtodwords"
+#else
+                  "binarytogw/gwtobinary"
+#endif
+                  " is not identity for S1 = %Zd\nResult: %Zd\ng = ", S1, t);
       gwdump (g1);
     }
+
+  /* Test for S2 */
+#ifdef USE_FCONV
   Fdwordstogw (PTR(S2), ABSIZ(S2), g1, gwplan);
   Fgwtodwords (g1, PTR(t), &SIZ(t), ALLOC(t), gwplan);
-  if (mpz_sgn (S2) < 0)
-    mpz_neg (t, t);
+#else
+  binarytogw (PTR(S2), ABSIZ(S2), g1);
+  SIZ(t) = gwtobinary (g1, PTR(t), ALLOC(t));
+#endif
   if (mpz_cmp (S2, t) != 0)
     {
-      gmp_printf ("Fdwordstogw/Fgwtodwords is not identity for %Zd\nResult: %Zd\ng = ", 
-                  S2, t);
+      gmp_printf (
+#ifdef USE_FCONV
+                  "Fdwordstogw/Fgwtodwords"
+#else
+                  "binarytogw/gwtobinary"
+#endif
+                  "is not identity for S2 = %Zd\nResult: %Zd\ng = ", S2, t);
       gwdump (g1);
     }
+//  printf ("Identitiy test passed\n");
   mpz_clear (t);
 #endif
 
   if (ALLOC(R) < (signed) FFTLEN / 2)
     _mpz_realloc (R, FFTLEN / 2);
 
+#ifdef USE_FCONV
   Fdwordstogw (PTR(S1), ABSIZ(S1), g1, gwplan);
+#else
+  binarytogw (PTR(S1), ABSIZ(S1), g1);
+#endif
   if (S1 == S2)
     gwsquare (g1);
   else
     {
-      sgnchg = mpz_sgn (S1) ^ mpz_sgn (S2);
+#ifdef USE_FCONV
       Fdwordstogw (PTR(S2), ABSIZ(S2), g2, gwplan);
+#else
+      binarytogw (PTR(S2), ABSIZ(S2), g2);
+#endif
       gwmul (g2, g1); /* g1 = g1 * g2, g2 left FFT'd */
     }
   if (gw_test_for_error () != 0)
     {
-      fprintf (stderr, "Fgwmul: gwsquare/gwmul reports error %ld\n", 
+      fprintf (stderr, "Fgwmul: gwsquare/gwmul reports error %d\n", 
                gw_test_for_error ());
       exit (EXIT_FAILURE);
     }
+#ifdef USE_FCONV
   Fgwtodwords (g1, PTR(R), &SIZ(R), ALLOC(R), gwplan);
-  if (sgnchg < 0)
+#else
+  SIZ(R) = gwtobinary (g1, PTR(R), ALLOC(R));
+#endif
+  ASSERT (SIZ(R) >= 0);
+
+  /* Undo sign change */
+  /* S1 == R or S2 == R is possible! Don't change sign of R here */
+  if (sgnS1 < 0 && S1 != R)
+    mpz_neg (S1, S1);
+  if (sgnS2 < 0 && S1 != S2 && S2 != R)
+    mpz_neg (S2, S2);
+  
+  /* If one source operand was negative and the other positive, 
+     change sign of R (also happens if both were 0, but changing sign has 
+     no effect then */
+  if (sgnS1 + sgnS2 == 0)
     mpz_neg (R, R);
 }
 
+int 
+gw_ecm_stage1 (mpz_t f, curve *P, mpmod_t modulus, 
+	       double B1, double *B1done, mpz_t go)
+{
+  const double gw_k = 1.;
+  const unsigned long gw_b = 2;
+  unsigned long gw_n = abs (modulus->bits);
+  signed long gw_c = sgn (modulus->bits);
+  unsigned long gw_B1done = *B1done;
+  unsigned long siz_x, siz_z; /* Size of gw_x and gw_y as longs */
+  mpz_t gw_x, gw_z, gw_A;
+  int youpi = ECM_NO_FACTOR_FOUND;
+
+  if (mpz_cmp_ui (go, 1) > 0)
+    {
+      mpres_t b;
+      mpres_init (b, modulus);
+      mpres_add_ui (b, P->A, 2, modulus);
+      mpres_div_2exp (b, b, 2, modulus); /* b == (A+2)/4 */
+      ecm_mul (P->x, P->y, go, modulus, b);
+      mpres_clear (b, modulus);
+    }
+  
+  outputf (OUTPUT_VERBOSE, 
+           "Using gwnum_ecmStage1(%f, %d, %d, %d, , %.0f, %ld)\n",
+           gw_k, gw_b, gw_n, gw_c, B1, gw_B1done);
+
+  /* Copy x, z and A values from modular representation to 
+     plain integers */
+
+  /* Allocate enough memory for any residue (mod 2^n+-1) for x, z */
+  mpz_init2 (gw_x, gw_n + 64);
+  mpz_init2 (gw_z, gw_n + 64);
+  mpz_init (gw_A);
+
+  /* mpres_get_z always produces non-negative integers */
+  mpres_get_z (gw_x, P->x, modulus);
+  mpres_get_z (gw_z, P->y, modulus);
+  mpres_get_z (gw_A, P->A, modulus);
+
+  /* Temporarily pause the use of GWNUM by mpmod.c */
+  mpmod_pausegw (modulus);
+
+  /* gwnum_ecmStage1() wants long int pointers for size_x, size_z, 
+     so copy them into long int vars */
+  siz_x = SIZ(gw_x);
+  siz_z = SIZ(gw_z);
+  
+  youpi = gwnum_ecmStage1 (gw_k, gw_b, gw_n, gw_c, 
+      PTR(modulus->orig_modulus), ABSIZ(modulus->orig_modulus), 
+      B1, &gw_B1done, PTR(gw_A), ABSIZ(gw_A), 
+      PTR(gw_x), &siz_x, PTR(gw_z), &siz_z, NULL, 0);
+  
+  SIZ(gw_x) = siz_x;
+  SIZ(gw_z) = siz_z;
+
+  /* Resume use of GWNUM by mpmod.c */
+  mpmod_contgw (modulus);
+
+  outputf (OUTPUT_DEVVERBOSE, 
+           "gw_ecm_stage1: after gwnum_ecmStage1, \n"
+           "B1done = %lu, x = %Zd\nz = %Zd\n",
+           gw_B1done, gw_x, gw_z);
+  
+  /* Copy x, z back to P and clean up the temp vars */
+  mpres_set_z (P->x, gw_x, modulus);
+  mpres_set_z (P->y, gw_z, modulus);
+  mpz_clear (gw_A);
+  mpz_clear (gw_z);
+  mpz_clear (gw_x);
+
+  *B1done = gw_B1done;
+
+  if (youpi > 1)
+    {
+      outputf (OUTPUT_ERROR, "GW stage 1 returned code %d\n", youpi);
+      youpi = ECM_ERROR;
+      goto end_of_gwecm;
+    }
+
+  if (youpi == 1)
+    {
+      /* How did that happen? Since we passed z, GWNUM should not do
+         an extgcd and so not find factors... but if it did anyways, 
+         we deal with it. Who's going to turn down a factor? */
+      outputf (OUTPUT_DEVVERBOSE, 
+               "gw_ecm_stage1: Strange, gwnum_ecmStage1 reports a factor\n");
+      mpres_get_z (f, P->x, modulus);
+      youpi = ECM_FACTOR_FOUND_STEP1;
+      goto end_of_gwecm;
+    }
+
+  /* Normalize z (in P->y) to 1 */
+  youpi = ECM_NO_FACTOR_FOUND;
+  if (!mpres_invert (P->y, P->y, modulus)) /* Factor found? */
+    {
+      mpres_gcd (f, P->y, modulus);
+      youpi = ECM_FACTOR_FOUND_STEP1;
+    } 
+  else
+    {
+      mpres_mul (P->x, P->x, P->y, modulus);
+      mpres_set_ui (P->y, 1, modulus);
+    }
+
+end_of_gwecm:
+
+  return youpi;
+}
 
 #endif /* endelse of #ifndef HAVE_GWNUM */
