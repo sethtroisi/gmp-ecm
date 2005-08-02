6d67ef069588d52410adfeef10555314ac48a19d
dnewman <dnewman@404564d9-a503-0410-82bf-e18ce2cf3989>
2005-08-02 16:30:21 +0000

    Changed test script shell to /bin/sh as MinGW doesn't have bash. Makefile.am now replaces the getprime2.c and random2.c hacks. Fixed CryptGenRandom() in random.c (maybe) and cleaned up the #includes a bit.

--- random.c
+++ random.c
@@ -22,17 +22,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#if !defined (_MSC_VER)
-#include <unistd.h> /* getpid */
-#include <sys/time.h> /* gettimeofday */
-#endif
-#include <time.h>
-
 #include <gmp.h>
+
 #ifdef OUTSIDE_LIBECM
-#include "ecm-ecm.h"
+# include "ecm-ecm.h"
 #else
-#include "ecm-impl.h"
+# include "ecm-impl.h"
+#endif
+
+#if HAVE_UNISTD_H
+# include <unistd.h> /* getpid */
+#endif
+
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+
+#if defined (_MSC_VER) || defined (__MINGW32__)
+# include <windows.h>
+# include <wincrypt.h>
+#endif
+
+#if 0  /* dirty hack until outputf gets fixed */
+#ifdef outputf
+# undef outputf
+# define outputf(x,y) printf(y)
+#endif
 #endif
 
 /* put in 'a' a valid random seed for P-1, i.e. gcd(a,n)=1 and a <> {-1,1} */
@@ -74,35 +96,45 @@ pp1_random_seed (mpz_t seed, mpz_t n, gmp_randstate_t randstate)
 }
 
 /* Produces a random unsigned int value */
+
 #if defined (_MSC_VER) || defined (__MINGW32__)
-#include <windows.h>
-#include <wincrypt.h>
 unsigned int 
 get_random_ui (void)
 {
   SYSTEMTIME tv;
-
   HCRYPTPROV Prov;
 
-  if (CryptAcquireContext(&Prov, NULL, NULL, PROV_RSA_FULL, 0))
-  {
-    int r;
-    unsigned int rnd;
+  if (CryptAcquireContext (&Prov, NULL, NULL, PROV_RSA_FULL,
+    CRYPT_VERIFYCONTEXT))
+    {
+      int r;
+      unsigned int rnd;
     
-    r = CryptGenRandom(Prov, sizeof(unsigned int), (void *) &rnd);
-    CryptReleaseContext(Prov, 0);
-    if (r)
-      return rnd;
-  }
+      r = CryptGenRandom (Prov, sizeof (unsigned int), (void *) &rnd);
+      CryptReleaseContext (Prov, 0);
+      if (r)
+        {
+#ifndef OUTSIDE_LIBECM /* warning: outputf is not exported from libecm */
+          outputf (OUTPUT_DEVVERBOSE, "Got seed for RNG from CryptGenRandom\n");
+#endif
+          return rnd;
+        }
+    }
   
-  GetSystemTime(&tv);
+#ifndef OUTSIDE_LIBECM /* warning: outputf is not exported from libecm */
+  outputf (OUTPUT_DEVVERBOSE, "Got seed for RNG from GetSystemTime\n");
+#endif
+
+  GetSystemTime (&tv);
   /* This gets us 27 bits of somewhat "random" data based on the time clock.
      It would probably do the program justice if a better random mixing was done
      in the non-MinGW get_random_ui if /dev/random does not exist */
   return ((tv.wHour<<22)+(tv.wMinute<<16)+(tv.wSecond<<10)+tv.wMilliseconds) ^
          ((tv.wMilliseconds<<17)+(tv.wMinute<<11)+(tv.wHour<<6)+tv.wSecond);
 }
+
 #else
+
 unsigned int 
 get_random_ui (void)
 {
@@ -114,7 +146,7 @@ get_random_ui (void)
   rndfd = fopen ("/dev/urandom", "r");
   if (rndfd != NULL)
     {
-      if (fread (&t, sizeof(unsigned int), 1, rndfd) == 1)
+      if (fread (&t, sizeof (unsigned int), 1, rndfd) == 1)
         {
 #ifndef OUTSIDE_LIBECM /* warning: outputf is not exported from libecm */
           outputf (OUTPUT_DEVVERBOSE, "Got seed for RNG from /dev/urandom\n");
@@ -125,6 +157,7 @@ get_random_ui (void)
       fclose (rndfd);
     }
 
+#if HAVE_GETTIMEOFDAY
   if (gettimeofday (&tv, NULL) == 0)
     {
 #ifndef OUTSIDE_LIBECM
@@ -132,6 +165,7 @@ get_random_ui (void)
 #endif
       return tv.tv_sec + tv.tv_usec;
     }
+#endif
 
 #ifndef OUTSIDE_LIBECM
   outputf (OUTPUT_DEVVERBOSE, "Got seed for RNG from time()+getpid()\n");
