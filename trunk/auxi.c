17c6b1d877f2c1b2e5c6cd5d56d817ed9c405681
zimmerma <zimmerma@404564d9-a503-0410-82bf-e18ce2cf3989>
2007-10-11 20:26:05 +0000

    fixed copyright line

--- trunk/auxi.c
+++ trunk/auxi.c
@@ -1,6 +1,6 @@
 /* Auxiliary functions for GMP-ECM.
 
-  Copyright 2002, 2003, 2004, 2005 Paul Zimmermann, Alexander Kruppa, Laurent Fousse, Jim Fougeron.
+  Copyright 2002, 2003, 2004, 2005, 2007 Paul Zimmermann, Alexander Kruppa, Laurent Fousse, Jim Fougeron.
 
   This program is free software; you can redistribute it and/or modify it
   under the terms of the GNU General Public License as published by the
