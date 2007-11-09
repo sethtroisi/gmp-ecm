2918bc500c786e04bb6b8fe2b0c923f2c6a7892f
Alexander Kruppa <akruppa@gmail.com>
2007-11-09 16:24:26 +0000

    Added mpmod_copy() to clone a mpmod_t, i.e. for threads
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ecm/trunk@1056 404564d9-a503-0410-82bf-e18ce2cf3989

--- mpmod.c
+++ mpmod.c
@@ -678,6 +678,24 @@ mpmod_clear (mpmod_t modulus)
   return;
 }
 
+
+void
+mpmod_copy (mpmod_t r, const mpmod_t modulus)
+{
+  r->repr = modulus->repr;
+  r->bits = modulus->bits;
+  r->Fermat = modulus->Fermat;
+  r->Nprim = modulus->Nprim;
+  mpz_init_set (r->orig_modulus, modulus->orig_modulus);
+  mpz_init_set (r->aux_modulus, modulus->aux_modulus);
+  mpz_init_set (r->multiple, modulus->multiple);
+  mpz_init_set (r->R2, modulus->R2);
+  mpz_init_set (r->R3, modulus->R3);
+  mpz_init2 (r->temp1, 2 * r->bits + __GMP_BITS_PER_MP_LIMB);
+  mpz_init2 (r->temp2, r->bits + __GMP_BITS_PER_MP_LIMB);
+}
+
+
 void 
 mpmod_pausegw (ATTRIBUTE_UNUSED const mpmod_t modulus)
 {
