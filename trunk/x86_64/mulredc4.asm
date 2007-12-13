db8fa7eebe0c5fa4e434815784bc269f5febead2
kruppa <kruppa@404564d9-a503-0410-82bf-e18ce2cf3989>
2007-12-13 14:24:22 +0000

    More optimization of mulredc, up to 4% faster

--- trunk/x86_64/mulredc4.asm
+++ trunk/x86_64/mulredc4.asm
@@ -57,20 +57,126 @@ GSYM_PREFIX`'mulredc4:
 	movq	%rsi, %r13		# store x in XP
 	movq	%rdx, %r9		# store y in YP
 	movq	%rcx, %r10		# store m in MP
-	xorq	%rax, %rax		# set %rax to 0
-	movq	%rax, %rcx		# Set CY to 0
 
-# Clear tmp memory
+
+#########################################################################
+# i = 0 pass
+#########################################################################
+
+# register values at loop entry: %TP = tmp, %I = i, %YP = y, %MP = m
+# %CY < 255 (i.e. only low byte may be != 0)
+
+# Pass for j = 0. We need to fetch x[i] from memory and compute the new u
+
+	movq	(%r13), %r14		# XI = x[0]
+	movq	(%r9), %rax		# rax = y[0]
+
+	xorq	%rcx, %rcx		# set %CY to 0
 	lea	(%rsp), %rbp		# store addr of tmp array in TP
-	movq	%rax, %r12 		# set I to 0
-	movq	%rax, (%rbp)
-	movq	%rax, 8(%rbp)
-	movq	%rax, 16(%rbp)
-	movq	%rax, 24(%rbp)
-	movq	%rax, 32(%rbp)
+	movq	%rcx, %r12			# Set %I to 0
+
+	mulq	%r14			# rdx:rax = y[0] * x[i]
+	addq	$1, %r12
+
+	movq 	%rax, %rsi		# Move low word of product to T0
+	movq	%rdx, %rbx		# Move high word of procuve to T1
+
+	imulq	%r8, %rax		# %rax = ((x[i]*y[0]+tmp[0])*invm)%2^64
+	movq	%rax, %r11		# this is the new u value
+
+	mulq	(%r10)			# multipy u*m[0]
+	addq	%rax, %rsi		# Now %T0 = 0, need not be stored
+	movq	8(%r9), %rax		# Fetch y[1]
+	adcq	%rdx, %rbx		# 
+	setc	%cl
+	# CY:T1:T0 <= 2*(2^64-1)^2 <= 2^2*128 - 4*2^64 + 2, hence
+	# CY:T1 <= 2*2^64 - 4
 
 
-.align 32
+# Now T0 = rbx, T1 = rsi
+
+
+# Pass for j = 1
+# Register values at entry: 
+# %rax = y[j], %r14 = x[i], %r11 = u
+# %rbp = tmp, %rbx = value to store in tmp[j], %rsi undefined 
+# %rcx = carry into rsi (is <= 2)
+# We have %CY:%T1 <= 2 * 2^64 - 2
+
+	movq	%rcx, %rsi	# T1 = CY <= 1
+
+	# Here, T1:T0 <= 2*2^64 - 2
+	mulq	%r14		# y[j] * x[i]
+	# rdx:rax <= (2^64-1)^2 <= 2^128 - 2*2^64 + 1
+	addq	%rax, %rbx	# Add low word to T0
+	movq	8(%r10), %rax	# Fetch m[j] into %rax
+	adcq	%rdx, %rsi	# Add high word with carry to T1
+	# T1:T0 <= 2^128 - 2*2^64 + 1 + 2*2^64 - 2 <= 2^128 - 1, no carry!
+	
+	mulq	%r11		# m[j]*u
+	# rdx:rax <= 2^128 - 2*2^64 + 1, T1:T0 <= 2^128 - 1
+	addq	%rbx, %rax	# Add T0 and low word
+	movq	%rax, 0(%rbp)	# Store rbx in tmp[1-1]
+	movq	16(%r9), %rax	# Fetch y[j+1] = y[2] into %rax
+	adcq	%rdx, %rsi	# Add high word with carry to T1
+	setc	%cl		# %CY <= 1
+	# CY:T1:T0 <= 2^128 - 1 + 2^128 - 2*2^64 + 1 <=
+	#             2 * 2^128 - 2*2^64 ==> CY:T1 <= 2 * 2^64 - 2
+
+# Now T0 = rsi, T1 = rbx
+
+
+# Pass for j = 2
+# Register values at entry: 
+# %rax = y[j], %r14 = x[i], %r11 = u
+# %rbp = tmp, %rsi = value to store in tmp[j], %rbx undefined 
+# %rcx = carry into rbx (is <= 2)
+# We have %CY:%T1 <= 2 * 2^64 - 2
+
+	movq	%rcx, %rbx	# T1 = CY <= 1
+
+	# Here, T1:T0 <= 2*2^64 - 2
+	mulq	%r14		# y[j] * x[i]
+	# rdx:rax <= (2^64-1)^2 <= 2^128 - 2*2^64 + 1
+	addq	%rax, %rsi	# Add low word to T0
+	movq	16(%r10), %rax	# Fetch m[j] into %rax
+	adcq	%rdx, %rbx	# Add high word with carry to T1
+	# T1:T0 <= 2^128 - 2*2^64 + 1 + 2*2^64 - 2 <= 2^128 - 1, no carry!
+	
+	mulq	%r11		# m[j]*u
+	# rdx:rax <= 2^128 - 2*2^64 + 1, T1:T0 <= 2^128 - 1
+	addq	%rsi, %rax	# Add T0 and low word
+	movq	%rax, 8(%rbp)	# Store rsi in tmp[2-1]
+	movq	24(%r9), %rax	# Fetch y[j+1] = y[3] into %rax
+	adcq	%rdx, %rbx	# Add high word with carry to T1
+	setc	%cl		# %CY <= 1
+	# CY:T1:T0 <= 2^128 - 1 + 2^128 - 2*2^64 + 1 <=
+	#             2 * 2^128 - 2*2^64 ==> CY:T1 <= 2 * 2^64 - 2
+
+# Now T0 = rbx, T1 = rsi
+
+
+# Pass for j = 3. Don't fetch new data from y[j+1].
+
+	movq	%rcx, %rsi	# T1 = CY <= 1
+	
+	mulq	%r14		# y[j] * x[i]
+	addq	%rax, %rbx	# Add low word to T0
+	movq	24(%r10), %rax	# Fetch m[j] into %rax
+	adcq	%rdx, %rsi 	# Add high word with carry to T1
+	mulq    %r11		# m[j]*u
+	addq	%rax, %rbx	# Add low word to T0
+	movq	%rbx, 16(%rbp)	# Store T0 in tmp[j-1]
+	adcq	%rdx, %rsi	# Add high word with carry to T1
+	movq	%rsi, 24(%rbp)	# Store T1 in tmp[j]
+	setc	%cl		# %CY <= 1
+	movq	%rcx, 32(%rbp)	# Store CY in tmp[j+1]
+
+#########################################################################
+# i > 0 passes
+#########################################################################
+
+.align 32,,16
 1:
 
 # register values at loop entry: %TP = tmp, %I = i, %YP = y, %MP = m
@@ -82,96 +188,96 @@ GSYM_PREFIX`'mulredc4:
 	movq	(%r13,%r12,8), %r14		# XI = x[i]
 	movq	(%r9), %rax		# rax = y[0]
 #init the register tmp ring buffer
-        movq	(%rbp), %rsi		# Load tmp[0] into T0
-	movq	8(%rbp), %rbx		# Load tmp[1] into T1
+        movq	(%rbp), %rbx		# Load tmp[0] into T0
+	movq	8(%rbp), %rsi		# Load tmp[1] into T1
 
 	mulq	%r14			# rdx:rax = y[0] * x[i]
 	addq	$1, %r12
 
-	addq	%rsi, %rax		# Add T0 to low word
-	adcq	%rdx, %rbx		# Add high word with carry to T1
+	addq	%rbx, %rax		# Add T0 to low word
+	adcq	%rdx, %rsi		# Add high word with carry to T1
 	setc	%cl			# %CY <= 1
 
-	movq 	%rax, %rsi		# Save sum of low words in T0
+	movq 	%rax, %rbx		# Save sum of low words in T0
 	imulq	%r8, %rax		# %rax = ((x[i]*y[0]+tmp[0])*invm)%2^64
 	movq	%rax, %r11		# this is the new u value
 
 	mulq	(%r10)			# multipy u*m[0]
-	addq	%rax, %rsi		# Now %T0 = 0, need not be stored
-	adcq	%rdx, %rbx		# 
+	addq	%rax, %rbx		# Now %T0 = 0, need not be stored
+	adcq	%rdx, %rsi		# 
 
 	movq	8(%r9), %rax		# Fetch y[1]
 
 
-# Now T0 = rbx, T1 = rsi
-
-
-# Pass for j = 1
-# Register values at entry: 
-# %rax = y[j], %r14 = x[i], %r11 = u
-# %rbp = tmp, %rbx = value to store in tmp[j], %rsi value to store in 
-# tmp[j+1], %rcx = carry into rsi, carry flag: also carry into rsi
-
-	movq	%rcx, %rsi	# T1 = CY
-	adcq	16(%rbp), %rsi	# T1 += tmp[j+1]
-	setc	%cl		# %CY <= 1
-
-	mulq	%r14		# y[j] * x[i]
-	addq	%rax, %rbx	# Add low word to T0
-	movq	8(%r10), %rax	# Fetch m[j] into %rax
-	adcq	%rdx, %rsi	# Add high word with carry to T1
-	adcb	$0, %cl	# %CY <= 2
-	
-	mulq	%r11		# m[j]*u
-	addq	%rbx, %rax	# Add T0 and low word
-	movq	%rax, 0(%rbp)	# Store rbx in tmp[1-1]
-	adcq	%rdx, %rsi	# Add high word with carry to T1
-	movq	16(%r9), %rax	# Fetch y[j+1] = y[2] into %rax
-
 # Now T0 = rsi, T1 = rbx
 
 
-# Pass for j = 2
+# Pass for j = 1
 # Register values at entry: 
 # %rax = y[j], %r14 = x[i], %r11 = u
 # %rbp = tmp, %rsi = value to store in tmp[j], %rbx value to store in 
 # tmp[j+1], %rcx = carry into rbx, carry flag: also carry into rbx
 
 	movq	%rcx, %rbx	# T1 = CY
-	adcq	24(%rbp), %rbx	# T1 += tmp[j+1]
+	adcq	16(%rbp), %rbx	# T1 += tmp[j+1]
 	setc	%cl		# %CY <= 1
 
 	mulq	%r14		# y[j] * x[i]
 	addq	%rax, %rsi	# Add low word to T0
-	movq	16(%r10), %rax	# Fetch m[j] into %rax
+	movq	8(%r10), %rax	# Fetch m[j] into %rax
 	adcq	%rdx, %rbx	# Add high word with carry to T1
 	adcb	$0, %cl	# %CY <= 2
 	
 	mulq	%r11		# m[j]*u
 	addq	%rsi, %rax	# Add T0 and low word
-	movq	%rax, 8(%rbp)	# Store rsi in tmp[2-1]
+	movq	%rax, 0(%rbp)	# Store rsi in tmp[1-1]
 	adcq	%rdx, %rbx	# Add high word with carry to T1
-	movq	24(%r9), %rax	# Fetch y[j+1] = y[3] into %rax
+	movq	16(%r9), %rax	# Fetch y[j+1] = y[2] into %rax
 
 # Now T0 = rbx, T1 = rsi
 
 
-# Pass for j = 3. Don't fetch new data from y[j+1].
+# Pass for j = 2
+# Register values at entry: 
+# %rax = y[j], %r14 = x[i], %r11 = u
+# %rbp = tmp, %rbx = value to store in tmp[j], %rsi value to store in 
+# tmp[j+1], %rcx = carry into rsi, carry flag: also carry into rsi
 
 	movq	%rcx, %rsi	# T1 = CY
-	adcq	32(%rbp), %rsi	# T1 += tmp[j + 1]
+	adcq	24(%rbp), %rsi	# T1 += tmp[j+1]
+	setc	%cl		# %CY <= 1
+
+	mulq	%r14		# y[j] * x[i]
+	addq	%rax, %rbx	# Add low word to T0
+	movq	16(%r10), %rax	# Fetch m[j] into %rax
+	adcq	%rdx, %rsi	# Add high word with carry to T1
+	adcb	$0, %cl	# %CY <= 2
+	
+	mulq	%r11		# m[j]*u
+	addq	%rbx, %rax	# Add T0 and low word
+	movq	%rax, 8(%rbp)	# Store rbx in tmp[2-1]
+	adcq	%rdx, %rsi	# Add high word with carry to T1
+	movq	24(%r9), %rax	# Fetch y[j+1] = y[3] into %rax
+
+# Now T0 = rsi, T1 = rbx
+
+
+# Pass for j = 3. Don't fetch new data from y[j+1].
+
+	movq	%rcx, %rbx	# T1 = CY
+	adcq	32(%rbp), %rbx	# T1 += tmp[j + 1]
 	setc	%cl	    	# %CY <= 1
 	
 	mulq	%r14		# y[j] * x[i]
-	addq	%rax, %rbx	# Add low word to T0
+	addq	%rax, %rsi	# Add low word to T0
 	movq	24(%r10), %rax	# Fetch m[j] into %rax
-	adcq	%rdx, %rsi 	# Add high word with carry to T1
+	adcq	%rdx, %rbx 	# Add high word with carry to T1
 	adcb	$0, %cl	# %CY <= 2
 	mulq    %r11		# m[j]*u
-	addq	%rax, %rbx	# Add low word to T0
-	movq	%rbx, 16(%rbp)	# Store T0 in tmp[j-1]
-	adcq	%rdx, %rsi	# Add high word with carry to T1
-	movq	%rsi, 24(%rbp)	# Store T1 in tmp[j]
+	addq	%rax, %rsi	# Add low word to T0
+	movq	%rsi, 16(%rbp)	# Store T0 in tmp[j-1]
+	adcq	%rdx, %rbx	# Add high word with carry to T1
+	movq	%rbx, 24(%rbp)	# Store T1 in tmp[j]
 	adcb	$0, %cl	# %CY <= 3
 	movq	%rcx, 32(%rbp)	# Store CY in tmp[j+1]
 
