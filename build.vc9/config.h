db6cfd5a5dd6a590934c748d3974780e1e4ed18d
Brian Gladman <riemannic@gmail.com>
2008-05-01 21:45:32 +0000

    update version (noticed by Paul)
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ecm/trunk@1254 404564d9-a503-0410-82bf-e18ce2cf3989

--- build.vc9/config.h
+++ build.vc9/config.h
@@ -1,6 +1,6 @@
 /* config.h.in.  Generated from configure.in by autoheader.  */
 
-#define VERSION "6.2-rc1"
+#define VERSION "6.2-rc2"
 
 #define PACKAGE_BUGREPORT "ecm-discuss@lists.gforge.inria.fr"
 
