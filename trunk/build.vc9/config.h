15a19c3128748476ffe8bd4f9c2ec8c2769164f0
zimmerma <zimmerma@404564d9-a503-0410-82bf-e18ce2cf3989>
2010-04-09 17:31:09 +0000

    bumped version number to 6.3-rc4 (just to try "make dist")

--- trunk/build.vc9/config.h
+++ trunk/build.vc9/config.h
@@ -1,6 +1,6 @@
 /* config.h.in.  Generated from configure.in by autoheader.  */
 
-#define VERSION "6.3-rc3"
+#define VERSION "6.3-rc4"
 
 #define PACKAGE_BUGREPORT "ecm-discuss@lists.gforge.inria.fr"
 
