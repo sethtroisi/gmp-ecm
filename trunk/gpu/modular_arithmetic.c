5a0bcbca1f3d2b5c53ae0717a109a6acd1ec9f3a
zimmerma <zimmerma@404564d9-a503-0410-82bf-e18ce2cf3989>
2010-09-10 14:41:33 +0000

    now stage1-c seems to give similar results as stage1 (but the efficiency can still be improved)

--- trunk/gpu/modular_arithmetic.c
+++ trunk/gpu/modular_arithmetic.c
@@ -26,7 +26,7 @@
 #include <gmp.h>
 #include <sys/cdefs.h>
 #include <assert.h>
-
+#include "modular_arithmetic.h"
 
 #define ONE ((mp_limb_t)1)
 #define MASK ((ONE<<52)-ONE)
@@ -39,89 +39,70 @@
 #define C2 20282409603651670423947251286016.0 /* 2^104 */
 
 
-/* computes size of N in base 2^52 */
+/* returns number of words of N in base 2^52 */
 unsigned long
-sizeAlloc (mpz_t N)
+dpn_size (mpz_t N)
 {
-  mpz_t tmp;
-  mpz_t exp;
-  mpz_t exp_tmp;
-  unsigned long n;
+  unsigned long n = mpz_sizeinbase (N, 2);
 
-  mpz_init(tmp);
-  mpz_init(exp);
-  mpz_init(exp_tmp);
-
-  mpz_ui_pow_ui(exp,2,52);
-  n = mpz_size(N); /* starts with (2^52)^sizeN */
-  while (mpz_cmp_ui(tmp,0)>=0) {
-    mpz_pow_ui(exp_tmp,exp,n);
-    mpz_sub(tmp,N,exp_tmp); /* N - (2^52)^i */
-    n++;
-  }
-  mpz_clear(tmp); mpz_clear(exp); mpz_clear(exp_tmp);
-  return n-1;
+  return (n + 51) / 52;
 }
 
-/* converts N from base 2^64 to base 2^52 */
+/* converts N from base 2^64 to base 2^52, where n is the (maximal) number of
+   words of N in base 2^52, and b[] was allocated with n words */
 void
-conversion64to52(double *b,mpz_t N,unsigned long n) {
-	unsigned long tabr[14] = {52,40,28,16,4,56,44,32,20,8,60,48,36,24};
-	unsigned long tabl[14] = {12,24,36,48,60,8,20,32,44,56,4,16,28,40};
-	unsigned long i,j,k;
+conversion64to52 (dpn_t b, mpz_t N, unsigned long n)
+{
+  unsigned long tabr[14] = {52,40,28,16,4,56,44,32,20,8,60,48,36,24};
+  unsigned long tabl[14] = {12,24,36,48,60,8,20,32,44,56,4,16,28,40};
+  unsigned long i,j,k;
 
-	for (i=0; i<n; i++)
-		b[i] = 0.0;
+  for (i=0; i<n; i++)
+    b[i] = 0.0;
 	
-	for (i=mpz_size(N); i<n; i++)
-		N->_mp_d[i] = 0;
+  for (i=mpz_size(N); i<n; i++)
+    N->_mp_d[i] = 0;
 	
-	j = 0; k = 0;
-	b[0] = (N->_mp_d[0])&MASK;
-	for (i=1; i<n; i++) {
-		if ((j%5 == 0) && (j != 0))
-			k++;
+  j = 0; k = 0;
+  b[0] = (N->_mp_d[0])&MASK;
+  for (i=1; i<n; i++) {
+    if ((j%5 == 0) && (j != 0))
+      k++;
 			
-		if (i%15 == 0) {
-			j = 0;
-			b[i] = ((N->_mp_d[i-k-1])>>12)&MASK;
-			b[i+1] = (N->_mp_d[i-k])&MASK;
-			i++; k++;
-		}
-		else {
-			b[i] = (((N->_mp_d[i-k-1])>>tabr[j])|((N->_mp_d[i-k])<<tabl[j]))&MASK;
-			j++;
-		}
-	}
+    if (i%15 == 0) {
+      j = 0;
+      b[i] = ((N->_mp_d[i-k-1])>>12)&MASK;
+      b[i+1] = (N->_mp_d[i-k])&MASK;
+      i++; k++;
+    }
+    else {
+      b[i] = (((N->_mp_d[i-k-1])>>tabr[j])|((N->_mp_d[i-k])<<tabl[j]))&MASK;
+      j++;
+    }
+  }
 }
 
 
-/* converts N from base 2^52 to mpz_t */
+/* converts {b, n} from base 2^52 to mpz_t M */
 void
-conversion52to64(double * b,unsigned long n,mpz_t M)
+conversion52to64 (dpn_t b, unsigned long n, mpz_t M)
 {
   unsigned long i;
-  mpz_t exp;
-  mpz_t exp_tmp;
 
-  mpz_init(exp);
-  mpz_init(exp_tmp);
-	
-  mpz_ui_pow_ui(exp,2,52);
-  for (i=0; i<n; i++) {
-    mpz_pow_ui(exp_tmp,exp,i);
-    mpz_mul_ui(exp_tmp,exp_tmp,b[i]);
-    mpz_add(M,M,exp_tmp);
-  }
-  
-  mpz_clear(exp);
-  mpz_clear(exp_tmp);
+  mpz_set_ui (M, (unsigned long) b[n-1]);
+  for (i = n - 1; i > 0;)
+    {
+      mpz_mul_2exp (M, M, 52);
+      mpz_add_ui (M, M, (unsigned long) b[--i]);
+    }
 }
 
 
 /* converts N from base 2^64 to base 2^52 */
 void
-conversion52to64bis(unsigned long *a,double *b,unsigned long sizeb,unsigned long n) {
+conversion52to64bis (unsigned long *a, dpn_t b, unsigned long sizeb,
+                     unsigned long n)
+{
 	unsigned long tabr[12] = {12,24,36,48,8,20,32,44,4,16,28,40};
 	unsigned long tabl[12] = {40,28,16,56,44,32,20,60,48,36,24,12};
 	unsigned long i,j,k,cmp;
@@ -166,52 +147,82 @@ conversion52to64bis(unsigned long *a,double *b,unsigned long sizeb,unsigned long
 
 /* {a, n} <- {b, n} + {c, n}, returns carry out (0 or 1) */
 double
-dpn_add(double *a,double *b,double *c,unsigned long n) {
-	unsigned long i;
-	double cy = 0.0;
-	for (i = 0; i < n; i++) {
-		cy += b[i] + c[i]; /* since 0 <= b[i], c[i] < 2^52, 0 <= cy < 2^53 */
-		if (cy >= TWO52) {
-			a[i] = cy - TWO52;
-			cy = 1.0;
-		}
-		else {
-			a[i] = cy;
-			cy = 0.0;
-		}
-	}
-	return cy;
+dpn_add (dpn_t a, dpn_t b, dpn_t c, unsigned long n)
+{
+  unsigned long i;
+  double cy = 0.0;
+
+  for (i = 0; i < n; i++)
+    {
+      cy += b[i] + c[i]; /* since 0 <= b[i], c[i] < 2^52, 0 <= cy < 2^53 */
+      if (cy >= TWO52)
+        {
+          a[i] = cy - TWO52;
+          cy = 1.0;
+        }
+      else
+        {
+          a[i] = cy;
+          cy = 0.0;
+        }
+    }
+  return cy;
 }
 
-
-
-/* {a, n} <- {b, n} - {c, n}, returns carry out (0 or 1) */
+/* {a, n} <- {b, n} + cy, where 0 <= cy <= 1, return carry out */
 double
-dpn_sub(double *a,double *b,double *c,unsigned long n) {
-	unsigned long i;
-	double cy = 0.0;
-	double t;
+dpn_add_1 (dpn_t a, dpn_t b, unsigned long n, double cy)
+{
+  unsigned long i;
 
-	for (i=0; i<n; i++) {
-		t = b[i]-c[i]-cy;
-		if (t >= 0) {
-			a[i] = t;
-			cy = 0.0;
-		}
-		else {
-			a[i] = t+TWO52;
-			cy = 1.0;
-		}
-	}
-
-	return cy;
+  for (i = 0; i < n; i++)
+    {
+      cy += b[i];
+      if (cy == TWO52)
+        {
+          a[i] = 0.0;
+          cy = 1.0;
+        }
+      else
+        {
+          a[i] = cy;
+          cy = 0.0;
+        }
+    }
+  return cy;
 }
 
+/* {a, n} <- {b, n} - {c, n}, returns borrow out (0 or 1) */
+double
+dpn_sub (dpn_t a, dpn_t b, dpn_t c, unsigned long n)
+{
+  unsigned long i;
+  double cy = 0.0;
 
+  for (i = 0; i < n; i++)
+    {
+      cy = b[i] - c[i] + cy; /* since 0 <= b[i], c[i] < 2^52 and -1 <= cy <= 0,
+                                we have -2^52 <= cy < 2^52 */
+                                
+      if (cy >= 0)
+        {
+          a[i] = cy;
+          cy = 0.0;
+        }
+      else
+        {
+          a[i] = cy + TWO52;
+          cy = -1.0;
+        }
+    }
+
+  return -cy;
+}
 
 /* ({a,n} > {b,n})?, returns 1(true), -1(false) or 0(a=b) */
 int
-dpn_cmp(double *a,double *b,unsigned long n) {
+dpn_cmp (dpn_t a, dpn_t b, unsigned long n)
+{
 	unsigned long i;
 	i = n;
 	while (i--) {
@@ -225,35 +236,41 @@ dpn_cmp(double *a,double *b,unsigned long n) {
 	return 0;
 }
 
-/* {a, n} <- {b, n} + {c, n} mod {mod, n}, returns carry out (0 or 1) */
-double
-dpn_add_mod(double *amod,double *b,double *c,double *mod,unsigned long n) {
-	double cy;
-	
-	cy = dpn_add(amod,b,c,n);
-	
-	while (dpn_cmp(amod,mod,n) >= 0) /* (a >= mod)? */
-		cy = dpn_sub(amod,amod,mod,n); /* {a, n} <- {a, n} - {mod, n} */
-		
-	return cy;	
-}
-
-
-		
-/* {a, n} <- {b, n} - {c, n} mod {mod, n}, returns carry out (0 or 1) */
-double
-dpn_sub_mod(double *smod,double *b,double *c,double *mod,unsigned long n)
+/* {a, n} <- {b, n} + {c, n} mod {mod, n}
+   Assumes 0 <= {b, n}, {c, n} < {mod, n} */
+void
+dpn_add_mod (dpn_t amod, dpn_t b, dpn_t c, dpn_t mod, unsigned long n)
 {
   double cy;
+
+  assert (dpn_check (b, mod, n));
+  assert (dpn_check (c, mod, n));
+  cy = dpn_add (amod, b, c, n);
 	
-  cy = dpn_sub(smod,b,c,n);
+  if (dpn_cmp (amod, mod, n) >= 0) /* (a >= mod)? */
+    cy = dpn_sub (amod, amod, mod, n); /* {a, n} <- {a, n} - {mod, n} */
+
+  assert (cy == 0.0);
+  assert (dpn_check (amod, mod, n));
+}
+		
+/* {a, n} <- {b, n} - {c, n} mod {mod, n}
+   Assumes 0 <= {b, n}, {c, n} < {mod, n} */
+void
+dpn_sub_mod (dpn_t smod, dpn_t b, dpn_t c, dpn_t mod, unsigned long n)
+{
+  double cy;
+
+  assert (dpn_check (b, mod, n));
+  assert (dpn_check (c, mod, n));
+  cy = dpn_sub (smod, b, c, n); /* 0 <= cy <= 1 */
 	
-  if (cy != 0) {
-    cy = dpn_add(smod,smod,mod,n);
-    if (cy == 0)
-      abort();
-  }
-  return cy;	
+  if (cy != 0.0) /* we should subtract 1 at smod[n] */
+    {
+      cy = dpn_add (smod, smod, mod, n);
+      assert (cy != 0.0);
+    }
+  assert (dpn_check (smod, mod, n));
 }
 
 
@@ -318,11 +335,12 @@ mul (double *h, double *l, double b, double c)
 /* precomputes b[i]*c[j], return hbc, lbc such that hbc[i][j]*2^52 + lbc[i][j] = b[i]*c[j]
    with 0 < b[i],c[j] < 2^52, 0 < hbc[i][j],lbc[i][j] < 2^52 */
 void
-coef(double **hbc,double **lbc,double *b,double *c,unsigned long n) {
+coef (dpn_t *hbc, dpn_t *lbc, dpn_t b, dpn_t c, unsigned long n)
+{
 	unsigned long i,j,k;
 	unsigned long nn = n*n;
-	double *h = (double*)malloc(nn*sizeof(double));
-	double *l = (double*)malloc(nn*sizeof(double));
+	dpn_t h = (dpn_t)malloc(nn*sizeof(double));
+	dpn_t l = (dpn_t)malloc(nn*sizeof(double));
 	/* initialization */
 	for (i=0; i<nn; i++) {
 		h[i] = 0.0; l[i] = 0.0;
@@ -347,15 +365,16 @@ coef(double **hbc,double **lbc,double *b,double *c,unsigned long n) {
 
 
 
-/* {a, n} <- {b, n} * {c, n}, returns carry out (0 or 1) */
-double
-dpn_mul(double *a,double *b,double *c,unsigned long n) {
+/* {a, 2n} <- {b, n} * {c, n} */
+void
+dpn_mul (dpn_t a, dpn_t b, dpn_t c, unsigned long n)
+{
 	unsigned long i,j,k;
 	unsigned long nn = 2*n;
 	double cy = 0.0;
 	
-	double ** hbc = malloc(nn*sizeof(double));
-	double ** lbc = malloc(nn*sizeof(double));
+	dpn_t *hbc = malloc(nn*sizeof(double));
+	dpn_t *lbc = malloc(nn*sizeof(double));
 	int cmp;
 
 	for (i=0; i<nn; i++)
@@ -421,104 +440,135 @@ dpn_mul(double *a,double *b,double *c,unsigned long n) {
 		free(hbc[i]); free(lbc[i]);
 	}
 	free(hbc); free(lbc);
-	
-	return cy;
+
+        assert (cy == 0.0);
 }
 
+/* {a, n} = {a, n} + {b, n} * c, returns carry out */
+double
+dpn_addmul_ui (dpn_t a, dpn_t b, unsigned long n, double c) 
+{
+  unsigned long i;
+  double cy = 0.0, h[1], l[1], t;
+  
+  for (i = 0; i < n; i++)
+    {
+      mul (h, l, b[i], c);
+      t = l[0] + cy; /* 0 <= cy < 2^53 */
+      if (t >= TWO52)
+        {
+          t -= TWO52;
+          cy = 1.0;
+        }
+      else
+        cy = 0.0;
+      t += a[i];
+      if (t >= TWO52)
+        {
+          t -= TWO52;
+          cy += 1.0;
+        }
+      a[i] = t;
+      cy += h[0];
+    }
+  return cy;
+}
 
+/* dpn_mod performs {a, n} <- {a, 2n} * (2^52)^(-n) mod N,
+   where N = {mod, n}, returns carry out (0 or 1) */
+void
+dpn_mod (dpn_t a, dpn_t mod, dpn_t mu, unsigned long n)
+{
+  unsigned long i,j,k;
+  double cy = 0.0;
+  unsigned long nn = 2*n;
+  dpn_t q = (dpn_t)malloc(nn*sizeof(double));
+  dpn_t r = (dpn_t)malloc(nn*sizeof(double));
+  dpn_t h = (dpn_t)malloc(nn*sizeof(double));
+  dpn_t l = (dpn_t)malloc(nn*sizeof(double));
+  dpn_t tmp = (dpn_t)malloc(nn*sizeof(double));
+  dpn_t tmp1 = (dpn_t)malloc(nn*sizeof(double));
+
+  /* initialization */
+  for (i=0; i<nn; i++) {
+    q[i] = 0.0; r[i] = 0.0; h[i] = 0.0; l[i] = 0.0; tmp[i] = 0.0; tmp1[i] = 0.0;
+  }
+
+  for (i = 0; i < n; i++)
+    {
+      mul (h+i, q+i, mu[0], a[i]);
+      /* tmp <- q[i]*mod */
+      mul (h+0, l+0, q[i], mod[0]);
+      h[0] = h[0] * TWOm52;
+    tmp[0] = l[0];
+    if (TWO52 < tmp[0]) {
+      tmp[0] = tmp[0]-TWO52;
+      cy++;
+    }
+    for (j=1; j<=n; j++) {
+      mul(h+j,l+j,q[i],mod[j]);
+      if (l[j] < 0) {
+        l[j] = l[j]+TWO52;
+        h[j] = h[j]-TWO52;
+      }
+      h[j] = h[j]*TWOm52;
+      tmp[j] = l[j]+h[j-1]+cy;
+      cy = 0.0;
+      if (TWO52 < tmp[j]) {
+        tmp[j] = tmp[j]-TWO52;
+        cy++;
+      }
+    }
+    /* q[i]*mod*(2^52)^i */
+    k = i;
+    for (j=0; j<nn-i; j++) {
+      tmp1[k] = tmp[j];
+      k++;
+    }
+    /* a <- a+q[i]*mod*(2^52)^i */
+    cy = dpn_add(a,a,tmp1,nn);
+    for (j=0; j<nn; j++) {
+      tmp[j] = 0.0; tmp1[j] = 0.0;
+    }
+  }
+
+  /* {a+n,n} >= mod? */
+  if (dpn_cmp (a + n, mod, n) >= 0)
+    {
+      cy = dpn_sub (a, a + n, mod, n); /* {a,n} <- {a+n,n} - {mod,n} */
+      assert (cy == 0.0);
+    }
+  else
+    dpn_set (a, a + n, n);
+
+  assert (dpn_check (a, mod, n));
+
+  free (q);
+  free (r);
+  free (h);
+  free (l);
+  free (tmp);
+  free (tmp1);
+}
 
 /* REDC algorithm computes {a, n} <- {b, n} * {c, n} * (2^52)^(-n),
    returns carry out (0 or 1) */
-double
-REDC(double *a,double *b,double *c,double *mod,double *mu,unsigned long n) {
-	unsigned long i,j,k;
-	double cy;
-	unsigned long nn = 2*n;
-	double *q = (double*)malloc(nn*sizeof(double));
-	double *r = (double*)malloc(nn*sizeof(double));
-	double *h = (double*)malloc(nn*sizeof(double));
-	double *l = (double*)malloc(nn*sizeof(double));
-	double *tmp = (double*)malloc(nn*sizeof(double));
-	double *tmp1 = (double*)malloc(nn*sizeof(double));
-	/* initialization */
-	for (i=0; i<nn; i++) {
-		q[i] = 0.0; r[i] = 0.0; h[i] = 0.0; l[i] = 0.0; tmp[i] = 0.0; tmp1[i] = 0.0;
-	}
-
-	cy = dpn_mul(a,b,c,n); /* a = b*c */
-
-	for (i=0; i<n; i++) {
-		mul(h+i,q+i,mu[0],a[i]);
-		if (q[i] < 0)
-			q[i] = q[i]+TWO52;
-		/* tmp <- q[i]*mod */
-		mul(h+0,l+0,q[i],mod[0]);
-		if (l[0] < 0) {
-			l[0] = l[0]+TWO52;
-			h[0] = h[0]-TWO52;
-		}
-		h[0] = h[0]*TWOm52;
-		tmp[0] = l[0];
-		if (TWO52 < tmp[0]) {
-			tmp[0] = tmp[0]-TWO52;
-			cy++;
-		}
-		for (j=1; j<=n; j++) {
-			mul(h+j,l+j,q[i],mod[j]);
-			if (l[j] < 0) {
-				l[j] = l[j]+TWO52;
-				h[j] = h[j]-TWO52;
-			}
-			h[j] = h[j]*TWOm52;
-			tmp[j] = l[j]+h[j-1]+cy;
-			cy = 0.0;
-			if (TWO52 < tmp[j]) {
-				tmp[j] = tmp[j]-TWO52;
-				cy++;
-			}
-		}
-		/* q[i]*mod*(2^52)^i */
-		k = i;
-		for (j=0; j<nn-i; j++) {
-			tmp1[k] = tmp[j];
-			k++;
-		}
-		/* a <- a+q[i]*mod*(2^52)^i */
-		cy = dpn_add(a,a,tmp1,nn);
-		for (j=0; j<nn; j++) {
-			tmp[j] = 0.0; tmp1[j] = 0.0;
-		}
-	}
-	/* r <- a*(2^52)^(-n) */
-	k = n;
-	for (i=0; i<n; i++) {
-		r[i] = a[k];
-		k++;
-	}
-	/* r >= (2^52)^n? */
-	tmp[n+1]=1; /* tmp=(2^52)^n */
-	if (dpn_cmp(r,tmp,nn) == 1)
-		cy = dpn_sub(a,r,mod,nn); /* a <- r-mod */
-	else {
-		for (i=0; i<nn; i++)
-			a[i] = r[i]; /* a <- r */
-	}
-	
-	free(q); free(r); free(h); free(l); free(tmp); free(tmp1);
-	return cy;
+void
+REDC (dpn_t a, dpn_t b, dpn_t c, dpn_t mod, dpn_t mu, unsigned long n)
+{
+  dpn_mul (a, b, c, n); /* a = b*c */
+  dpn_mod (a, mod, mu, n);
 }
-
-
 
 /* {a, n} <- {b, n} * {c, n} mod {mod, n}, returns carry out (0 or 1) */
 void
-dpn_mul_mod(double *a,double *b,double *c,mpz_t mod,double *mu,unsigned long n) {
-	double cy;
+dpn_mul_mod (dpn_t a, dpn_t b, dpn_t c, mpz_t mod, dpn_t mu, unsigned long n)
+{
 	unsigned long i;
 	unsigned long nn = 2*n;
-	double *redc = (double*)malloc(nn*sizeof(double));
-	double *un = (double*)malloc(n*sizeof(double));
-	double *d = (double*)malloc(n*sizeof(double));
+	dpn_t redc = (dpn_t)malloc(nn*sizeof(double));
+	dpn_t un = (dpn_t)malloc(n*sizeof(double));
+	dpn_t d = (dpn_t)malloc(n*sizeof(double));
 	mpz_t lambda;
 	mpz_t exp;
 	mpz_t A;
@@ -537,7 +587,7 @@ dpn_mul_mod(double *a,double *b,double *c,mpz_t mod,double *mu,unsigned long n) 
         mpz_init(exp);
         mpz_init(A);
 		
-	cy = dpn_mul(a,b,c,n); /* a = b*c */
+	dpn_mul(a,b,c,n); /* a = b*c */
 
 	conversion52to64(a,nn,A); /* converts a from base 2^52 to mpz_t (a=A) */
 	mpz_ui_pow_ui(exp,2,52);
@@ -548,14 +598,89 @@ dpn_mul_mod(double *a,double *b,double *c,mpz_t mod,double *mu,unsigned long n) 
 	
 	for (i=0; i<nn; i++)
 		a[i] = 0.0;
-	cy = REDC(a,redc,un,d,mu,n); /* computes (lambda*A mod[mod])*(2^52)^(-n) = b*c mod[mod] */
+	REDC(a,redc,un,d,mu,n); /* computes (lambda*A mod[mod])*(2^52)^(-n) = b*c mod[mod] */
 	
 	mpz_clear(lambda); mpz_clear(exp); mpz_clear(A);
 	free(redc); free(un); free(d);
 }
 
+/* {a, n} <- {b, n} */
+void
+dpn_set (dpn_t a, dpn_t b, unsigned long n)
+{
+  unsigned long i;
 
+  for (i = 0; i < n; i++)
+    a[i] = b[i];
+}
 
+/* {a, n} <- 0 */
+void
+dpn_zero (dpn_t a, unsigned long n)
+{
+  unsigned long i;
+
+  for (i = 0; i < n; i++)
+    a[i] = 0.0;
+}
+
+void
+dpn_print (dpn_t a, unsigned long n)
+{
+  unsigned long i;
+
+  printf ("%1.0f", a[0]);
+  for (i = 1; i < n; i++)
+    printf ("+%1.0f*2^%lu", a[i], 52*i);
+  printf ("\n");
+}
+
+int
+dpn_check (dpn_t a, dpn_t N, unsigned long n)
+{
+  unsigned long i;
+
+  for (i = 0; i < n; i++)
+    if (a[i] < 0.0 || a[i] >= TWO52 || a[i] != (double) (unsigned long) a[i])
+      return 0;
+  if (dpn_cmp (a, N, n) >= 0)
+    return 0;
+  return 1;
+}
+
+/* puts a into b-bit word Montgomery form, i.e., a <- a*2^(b*n) mod N,
+   where n is the number of words of N in base 2^b */
+void
+mpz_to_montgomery (mpz_t a, mpz_t N, unsigned long b)
+{
+  unsigned long n = mpz_sizeinbase (N, 2);
+
+  n = (n + b - 1) / b; /* ceil(n/b) */
+  mpz_mul_2exp (a, a, n * b);
+  mpz_mod (a, a, N);
+}
+
+/* puts a from b-bit word Montgomery form to classical form,
+   i.e., a <- a/2^(b*n) mod N,
+   where n is the number of words of N in base 2^b
+   Assumes N is odd.
+*/
+void
+mpz_from_montgomery (mpz_t a, mpz_t N, unsigned long b)
+{
+  unsigned long n = mpz_sizeinbase (N, 2), i;
+
+  n = (n + b - 1) / b; /* ceil(n/b) */
+  for (i = 0; i < n * b; i++)
+    {
+      if (mpz_divisible_ui_p (a, 2) == 0)
+        mpz_add (a, a, N);
+      /* now a is even */
+      mpz_div_2exp (a, a, 1);
+    }
+}
+
+#ifdef MAIN
 int
 main (int argc, char * argv[]) {
 	/* assume 0 < N,N1 < N2 */
@@ -565,23 +690,15 @@ main (int argc, char * argv[]) {
 	unsigned long nb,nc,nd;
 	unsigned long n;
 	unsigned long i;
-        double *b, *c, *d;
+        dpn_t a, b, c, d, s, ma, ms, mm, mmod, mu, un;
         unsigned long *test;
 	double cy;
-        double *a;
 	mpz_t add;
-        double *s;
 	mpz_t sub;
-        double *ma;
 	mpz_t moda;
-        double *ms;
 	mpz_t mods;
         unsigned long maxm;
-        double *mm;
 	mpz_t mult;
-        double *mmod;
-        double *mu;
-        double *un;
 	mpz_t lambda;
 	mpz_t tmp;
 	mpz_t exp;
@@ -600,14 +717,14 @@ main (int argc, char * argv[]) {
         mpz_init(N2);
         mpz_set_str(N2,argv[3],10); /* N2 -> modulo */
 
-	nb = sizeAlloc(N); nc = sizeAlloc(N1); nd = sizeAlloc(N2);
+	nb = dpn_size(N); nc = dpn_size(N1); nd = dpn_size(N2);
 	
 	n = (nc>nb)?nc:nb;
 	n = (n>nd)?n:nd;
 	
-	b = (double*)malloc(n*sizeof(double));
-	c = (double*)malloc(n*sizeof(double));
-	d = (double*)malloc(n*sizeof(double));
+	b = (dpn_t)malloc(n*sizeof(double));
+	c = (dpn_t)malloc(n*sizeof(double));
+	d = (dpn_t)malloc(n*sizeof(double));
 	for (i=0; i<n; i++) {
 		b[i] = 0.0; c[i] = 0.0; d[i] = 0.0;
 	}
@@ -634,7 +751,7 @@ main (int argc, char * argv[]) {
 		
 		
 	/* add */
-	a = (double*)malloc(n*sizeof(double));
+	a = (dpn_t)malloc(n*sizeof(double));
 	for (i=0; i<n; i++)
 		a[i] = 0.0;
 	cy = dpn_add(a,b,c,n);
@@ -647,7 +764,7 @@ main (int argc, char * argv[]) {
 	
 	
 	/* sub */
-	s = (double*)malloc(n*sizeof(double));
+	s = (dpn_t)malloc(n*sizeof(double));
 	for (i=0; i<n; i++)
 		s[i] = 0.0;
 	cy = dpn_sub(s,b,c,n);
@@ -660,10 +777,10 @@ main (int argc, char * argv[]) {
 	
 	
 	/* add mod */
-	ma = (double*)malloc(n*sizeof(double));
+	ma = (dpn_t)malloc(n*sizeof(double));
 	for (i=0; i<n; i++)
 		ma[i] = 0.0;
-	cy = dpn_add_mod(ma,b,c,d,n);
+	dpn_add_mod (ma, b, c, d, n);
 	for (i=0; i<n; i++)
 		printf("ma[%lu]=%f\n",i,ma[i]);
 	/* verif */
@@ -673,7 +790,7 @@ main (int argc, char * argv[]) {
 	
 	
 	/* sub mod */
-	ms = (double*)malloc(n*sizeof(double));
+	ms = (dpn_t)malloc(n*sizeof(double));
 	for (i=0; i<n; i++)
 		ms[i] = 0.0;
 	cy = dpn_sub_mod(ms,b,c,d,n);
@@ -687,7 +804,7 @@ main (int argc, char * argv[]) {
 	
 	/* mul */
 	maxm = 2*n;
-	mm = (double*)malloc(maxm*sizeof(double));
+	mm = (dpn_t)malloc(maxm*sizeof(double));
 	for (i=0; i<maxm; i++)
 		mm[i] = 0.0;
 	cy = dpn_mul(mm,b,c,n);
@@ -700,18 +817,18 @@ main (int argc, char * argv[]) {
 	
 	
 	/* mul mod */
-	mmod = (double*)malloc(maxm*sizeof(double));
+	mmod = (dpn_t)malloc(maxm*sizeof(double));
 	for (i=0; i<maxm; i++)
 		mmod[i] = 0.0;
-	mu = (double*)malloc(sizeof(double));
-	un = (double*)malloc(n*sizeof(double));
+	mu = (dpn_t)malloc(sizeof(double));
+	un = (dpn_t)malloc(n*sizeof(double));
 	un[0] = 1.0;
 	
         mpz_init(lambda);
         mpz_init(tmp);
         mpz_init(exp);
 	mpz_ui_pow_ui(exp,2,52);
-		/* mu = -N2^(-1) mod[2^52] */
+		/* mu = -1/N2 mod[2^52] */
 	mpz_mul_ui(tmp,N2,-1);
 	mpz_powm_ui(tmp,tmp,-1,exp);
 	conversion64to52(mu,tmp,1);
@@ -734,4 +851,4 @@ main (int argc, char * argv[]) {
 
         return 0;
 }
-
+#endif
