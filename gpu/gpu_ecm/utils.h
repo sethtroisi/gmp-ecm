8b4c205c01a7d914c54d8d2d87a2f8854665a929
bouvierc <bouvierc@404564d9-a503-0410-82bf-e18ce2cf3989>
2011-11-04 16:35:53 +0000

    New version of gpu_ecm (compatible with Fermi cards)

--- gpu/gpu_ecm/utils.h
+++ gpu/gpu_ecm/utils.h
@@ -5,6 +5,7 @@
 //#define DUP 5.0 // number of multiplications in a duplicate 
 
 void usage (void);
+long cputime();
 unsigned int findfactor(mpz_t N, mpz_t xfin, mpz_t zfin);
 void biguint_print (biguint_t a);
 void bigint_print (dbigint_t a);
@@ -13,5 +14,5 @@ void biguint_to_mpz (mpz_t a, biguint_t b);
 unsigned long getprime (unsigned long pp);
 void compute_s (mpz_t s, unsigned int B1);
 void write_resumefile_line (FILE *savefile, mpz_t N, unsigned int B1, mpz_t xp, 
-	unsigned int firstinvd, mpz_t mpz_d);
-
+                  unsigned int firstinvd, mpz_t mpz_d);
+int read_number (mpz_t n, FILE *fd);
