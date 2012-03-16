df71647e450cbbed4e759d7d9f37454b18888b83
bouvierc <bouvierc@404564d9-a503-0410-82bf-e18ce2cf3989>
2012-03-16 17:08:34 +0000

    Add licence in batch.c and ellparam_batch.c

--- batch.c
+++ batch.c
@@ -1,3 +1,26 @@
+/* batch.c - Implemente batch mode for step 1 of ECM
+ 
+Copyright 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011,
+2012 Paul Zimmermann, Alexander Kruppa and Cyril Bouvier.
+ 
+This file is part of the ECM Library.
+
+The ECM Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The ECM Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the ECM Library; see the file COPYING.LIB.  If not, see
+http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
+51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
+
+
 /* ECM stage 1 in batch mode, for initial point (x:z) with small coordinates,
    such that x and z fit into a mp_limb_t.
    For example we can start with (x=2:y=1) with the curve by^2 = x^3 + ax^2 + x
