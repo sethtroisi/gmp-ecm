7575b71853bd3bf5146a5e55f74046fa42cd3aeb
Brian Gladman <riemannic@gmail.com>
2012-04-17 20:25:44 +0000

    1. change the way that the Visual Studio solution picks up tuning parameters 2 move out of order declarations in parametrizations.c
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ecm/trunk@1944 404564d9-a503-0410-82bf-e18ce2cf3989

---
+++ build.vc10/mul_fft-params.h
@@ -0,0 +1,12 @@
+
+#define HAVE_MPIR
+
+#if 0 
+# include "..\mul_fft-params.h.athlon64"
+#elif 0
+# include "..\mul_fft-params.h.pentium3"
+#elif 0
+# include "..\mul_fft-params.h.pentium4"
+#else
+# include "..\mul_fft-params.h.default"
+#endif
