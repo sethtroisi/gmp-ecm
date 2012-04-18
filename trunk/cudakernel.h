3b81ec39ce6b8aa5ef84c12b81b0d1a2e4a3530e
bouvierc <bouvierc@404564d9-a503-0410-82bf-e18ce2cf3989>
2012-04-18 15:09:48 +0000

    [cudakernel.h|cu] Add ECM_GPU_ in front of #define constant [Makefile.am] Add cudakernel.h in rule for making CUDA code.

--- trunk/cudakernel.h
+++ trunk/cudakernel.h
@@ -2,11 +2,27 @@
 #define _CUDAKERNEL_H 1
 
 #ifdef GPU_CC20
-  #define MAJOR 2
-  #define MINOR 0
-  #define CURVES_BY_BLOCK 32//16
-  #define BLOCKS_BY_MP 1//3
-  #define CURVES_BY_MP CURVES_BY_BLOCK*BLOCKS_BY_MP
+  #define ECM_GPU_MAJOR 2
+  #define ECM_GPU_MINOR 0
+  #define ECM_GPU_CURVES_BY_BLOCK 32
+  #define ECM_GPU_BLOCKS_BY_MP 1
+  #define ECM_GPU_CURVES_BY_MP ECM_GPU_CURVES_BY_BLOCK * ECM_GPU_BLOCKS_BY_MP
+#endif
+
+#ifdef GPU_CC21
+  #define ECM_GPU_MAJOR 2
+  #define ECM_GPU_MINOR 1
+  #define ECM_GPU_CURVES_BY_BLOCK 32
+  #define ECM_GPU_BLOCKS_BY_MP 1
+  #define ECM_GPU_CURVES_BY_MP ECM_GPU_CURVES_BY_BLOCK * ECM_GPU_BLOCKS_BY_MP
+#endif
+
+#ifdef GPU_CC30
+  #define ECM_GPU_MAJOR 3
+  #define ECM_GPU_MINOR 0
+  #define ECM_GPU_CURVES_BY_BLOCK 32
+  #define ECM_GPU_BLOCKS_BY_MP 1
+  #define ECM_GPU_CURVES_BY_MP ECM_GPU_CURVES_BY_BLOCK * ECM_GPU_BLOCKS_BY_MP
 #endif
 
 __global__ void Cuda_Ell_DblAdd (biguint_t *xarg, biguint_t *zarg, 
