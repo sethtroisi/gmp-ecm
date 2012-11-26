a4c64b0bea406b719da2438b446da1621dd242d7
Francois Morain <morain@lix.polytechnique.fr>
2012-11-26 20:35:09 +0000

    Advances in the Hessian stuff, not finished.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ecm/trunk@2269 404564d9-a503-0410-82bf-e18ce2cf3989

--- ecm-impl.h
+++ ecm-impl.h
@@ -421,7 +421,7 @@ void duplicate (mpres_t, mpres_t, mpres_t, mpres_t, mpmod_t, mpres_t, mpres_t,
 void ecm_mul (mpres_t, mpres_t, mpz_t, mpmod_t, mpres_t);
 #define print_B1_B2_poly __ECM(print_B1_B2_poly)
 void print_B1_B2_poly (int, int, double, double, mpz_t, mpz_t, mpz_t, int S,  
-                       mpz_t, int, mpz_t, int, unsigned int);
+                       mpz_t, int, int, mpz_t, int, unsigned int);
 #define set_stage_2_params __ECM(set_stage_2_params)
 int set_stage_2_params (mpz_t, mpz_t, mpz_t, mpz_t, root_params_t *, double, 
                         double, unsigned long *, const int, int, int *,
