062f084aabf9422941df92ec1a19cf56895672f0
morain <morain@404564d9-a503-0410-82bf-e18ce2cf3989>
2013-01-26 16:38:16 +0000

    Implementation of ecm_rootsF for the CM continuation. Having some rest before attacking ecm_rootsG...

--- trunk/cmecm.h
+++ trunk/cmecm.h
@@ -1,2 +1,3 @@
 int build_curves_with_CM(mpz_t f, int *nE, ec_curve_t *tE, ec_point_t *tP, int disc, mpmod_t n, mpz_t *sqroots);
-int ecm_rootsF_CM(mpz_t f, listz_t F, root_params_t *root_params, unsigned long dF, curve *C, mpmod_t modulus);
+unsigned long compute_dF_CM(mpz_t B2, int disc);
+int ecm_rootsF_CM(mpz_t f, listz_t F, unsigned long dF, curve *C, mpmod_t modulus, double B1, mpz_t B2);
