f9e2ff62d03da575a8c8191d4bee3b2ba4e42307
morain <morain@404564d9-a503-0410-82bf-e18ce2cf3989>
2013-01-27 16:24:42 +0000

    Slowly advancing on the stage2 continuation for CM curves.

--- cmecm.h
+++ cmecm.h
@@ -1,3 +1,3 @@
 int build_curves_with_CM(mpz_t f, int *nE, ec_curve_t *tE, ec_point_t *tP, int disc, mpmod_t n, mpz_t *sqroots);
 unsigned long compute_dF_CM(mpz_t B2, int disc);
-int ecm_rootsF_CM(mpz_t f, listz_t F, unsigned long dF, curve *C, mpmod_t modulus, double B1, mpz_t B2);
+int ecm_rootsF_CM(mpz_t f, listz_t F, unsigned long dF, curve *C, mpmod_t modulus);
