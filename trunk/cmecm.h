aa253701c8da6f93626e5da359b90861301ffe99
morain <morain@404564d9-a503-0410-82bf-e18ce2cf3989>
2013-01-31 17:31:53 +0000

    Various fixes to make CMECM work in the case D=-4. Now testing.

--- trunk/cmecm.h
+++ trunk/cmecm.h
@@ -1,3 +1,4 @@
 int build_curves_with_CM(mpz_t f, int *nE, ec_curve_t *tE, ec_point_t *tP, int disc, mpmod_t n, mpz_t *sqroots);
-unsigned long compute_dF_CM(mpz_t B2, int disc);
+void
+set_stage2_params_CM(unsigned long *pdF, unsigned long *pk, mpz_t B2, int disc);
 int ecm_rootsF_CM(mpz_t f, listz_t F, unsigned long dF, curve *C, mpmod_t modulus);
