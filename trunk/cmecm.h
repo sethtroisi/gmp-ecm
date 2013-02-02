fb066f6a2dbf0a39eef49a452f6c39b5d7d5e196
morain <morain@404564d9-a503-0410-82bf-e18ce2cf3989>
2013-02-02 16:46:39 +0000

    A first attempt at a faster version for D=-4. To be continued.

--- trunk/cmecm.h
+++ trunk/cmecm.h
@@ -1,3 +1,5 @@
+#define CMECM_D_4 1 /* fast version */
+
 int build_curves_with_CM(mpz_t f, int *nE, ec_curve_t *tE, ec_point_t *tP, int disc, mpmod_t n, mpz_t *sqroots);
 void
 set_stage2_params_CM(unsigned long *pdF, unsigned long *pk, mpz_t B2, int disc);
