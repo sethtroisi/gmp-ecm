52fc982aaa81d1f686a03bdffe624ea55b812b45
Paul Zimmerman <paul.zimmerman@inria.fr>
2015-01-29 08:10:34 +0000

    script to perform coverage tests
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ecm/trunk@2543 404564d9-a503-0410-82bf-e18ce2cf3989

---
+++ coverage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# script to perform a coverage test
+t=$1 # target directory for the html files
+d=`mktemp -d /tmp/ecmXXX`
+cd $d
+svn checkout svn://scm.gforge.inria.fr/svnroot/ecm/trunk ecm
+cd ecm
+autoreconf -i
+./configure
+make CFLAGS="-O0 -g -fprofile-arcs -ftest-coverage"
+make check
+geninfo --no-checksum --ignore-errors gcov,source -q --output-filename ecm.info ./ --no-external
+rm -rf $t
+genhtml -o $t/ ecm.info
+cd
+rm -rf $d
