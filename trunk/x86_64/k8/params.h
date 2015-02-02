8c16dfba33fabc931467046fd0a3801143f6683c
zimmerma <zimmerma@404564d9-a503-0410-82bf-e18ce2cf3989>
2015-02-02 16:20:18 +0000

    improve sliding window exponentiation for P-1: use base-2 arithmetic when faster

--- trunk/x86_64/k8/params.h
+++ trunk/x86_64/k8/params.h
@@ -3,9 +3,9 @@
 #ifndef HAVE_MPIR /* tuning parameters for GMP, tuned with GMP 5.0.4 */
 
 /* 0:mulredc 1:mul+redc_1 2:mul+redc_2 3:mul+redc_n */
-#define TUNE_MULREDC_TABLE {0,0,0,0,0,0,0,0,0,0,0,0,2,0,2,1,2,1,1,1,2}
+#define TUNE_MULREDC_TABLE {0,0,0,0,0,0,0,0,0,1,1,1,0,1,0,1,1,1,1,1,1}
 /* 0:mulredc 1:sqr+redc_1 2:sqr+redc_2 3:sqr+redc_n */
-#define TUNE_SQRREDC_TABLE {0,0,0,0,0,0,0,0,2,2,1,2,2,1,2,1,2,1,1,1,2}
+#define TUNE_SQRREDC_TABLE {0,0,0,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,2,1,1}
 #define MPZMOD_THRESHOLD 21
 #define REDC_THRESHOLD 512
 #define MPN_MUL_LO_THRESHOLD_TABLE {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 9, 10, 11, 10, 12, 12, 12, 14, 14, 16, 16, 16, 18, 19, 19, 20, 21, 18, 19}
