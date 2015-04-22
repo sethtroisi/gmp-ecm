e67bf19eba861af0a12fda1887c390b24523a297
zimmerma <zimmerma@404564d9-a503-0410-82bf-e18ce2cf3989>
2015-04-22 13:58:32 +0000

    improve coverage

--- trunk/resume.c
+++ trunk/resume.c
@@ -182,20 +182,14 @@ read_resumefile_line (int *method, mpz_t x, mpz_t y, mpcandi_t *n,
           if (strcmp (tag, "METHOD") == 0)
             {
               if (facceptstr (fd, "ECM") == 3)
-                {
-                  *method = ECM_ECM;
-                }
-              else if (facceptstr (fd, "P"))
+                *method = ECM_ECM;
+              else if (facceptstr (fd, "P-1") == 3)
                 {
                   a = facceptstr (fd, "-1");
                   if (a == 2)
-                    {
-                      *method = ECM_PM1;
-                    }
+                    *method = ECM_PM1;
                   else if (a == 0 && facceptstr (fd, "+1") == 2)
-                    {
-                      *method = ECM_PP1;
-                    }
+                    *method = ECM_PP1;
                   else 
                     goto error;
                 }
