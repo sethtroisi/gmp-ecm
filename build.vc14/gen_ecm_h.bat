f6af3d99c34e4ab134322e15b805b6f24295896b
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2022-05-05 13:38:22 +0200

    switch version to 7.0.6-dev and enable assertions by default

--- build.vc14/gen_ecm_h.bat
+++ build.vc14/gen_ecm_h.bat
@@ -5,7 +5,7 @@ echo /* generated from ecm-h.in by gen_ecm_h.bat */>tmp.h
 for /f "tokens=1,2*" %%a in (..\ecm.h.in) do (
   if "%%a" EQU "#undef" (
     if "%%b" EQU "ECM_VERSION" (
-      echo #define ECM_VERSION "7.0.5">>tmp.h
+      echo #define ECM_VERSION "7.0.6-dev">>tmp.h
     )
   ) else echo %%a %%b %%c>>tmp.h
 )
