7898ae403da0c31ae697d713b430d52184a0c2a4
Seth Troisi <sethtroisi@google.com>
2023-09-13 20:15:59 -0700

    Simplify gpu_ecm api by passing ecm_params struct

--- factor.c
+++ factor.c
@@ -168,14 +168,7 @@ ecm_factor (mpz_t f, mpz_t n, double B1, ecm_params p0)
         }
       else
         {
-          res = gpu_ecm (f, p->x, p->param, p->sigma, n, p->go,
-                         &(p->B1done), B1, p->B2min, p->B2, p->k,
-                         p->S, p->verbose, p->repr, p->nobase2step2, 
-                         p->use_ntt, p->sigma_is_A, p->os, p->es,
-                         p->chkfilename, p->TreeFilename, p->maxmem,
-                         p->stop_asap, p->batch_s, &(p->batch_last_B1_used),
-                         p->gpu_cgbn, p->gpu_device, &(p->gpu_device_init),
-                         &(p->gpu_number_of_curves));
+          res = gpu_ecm (f, p, n, B1);
         }
 #endif
     }
