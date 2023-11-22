0cbaa55605f9e67d657187f9e859c6a88da27b53
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2023-11-22 14:38:41 +0100

    changed the license of resume.c to the MIT license
    This is done with the agreement of Alexander Kruppa and Cyril Bouvier,
    who are the main authors of this file.

--- resume.c
+++ resume.c
@@ -1,22 +1,27 @@
-/* Functions for reading a writing resume file lines.
+/* Functions for reading/writing resume file lines.
 
-Copyright 2001, 2002, 2003, 2004, 2005, 2006, 2008, 2010, 2011, 2012
-Paul Zimmermann, Alexander Kruppa and Cyril Bouvier.
+Copyright 2001-2023 Paul Zimmermann, Alexander Kruppa and Cyril Bouvier.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or (at your
-option) any later version.
+This file is distributed under the MIT license.
 
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-more details.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-You should have received a copy of the GNU General Public License
-along with this program; see the file COPYING.  If not, see
-http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
-51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
