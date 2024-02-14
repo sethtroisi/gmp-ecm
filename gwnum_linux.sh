a8823b6838d05abed38fddbdf8add079a548cf03
Phil McLaughlin <pbmcl01@protonmail.com>
2024-02-14 13:08:33 -0700

    Update to and test with P95 version 30.19 build 10

--- gwnum_linux.sh
+++ gwnum_linux.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-P95_URL=https://www.mersenne.org/download/software/v30/30.8/p95v308b15.source.zip
+P95_URL=https://mersenne.org/download/software/v30/30.19/p95v3019b10.source.zip
+echo "Loading P95 version 30.19 build 10"
 [ -d "/tmp/P95/" ] && rm -r /tmp/P95
 mkdir /tmp/P95
 wget -q -O /tmp/P95/P95_source.zip $P95_URL
