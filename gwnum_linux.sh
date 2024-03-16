6497399cb17558363d30ccae6a1a7b1e00fafc39
Phil McLaughlin <pbmcl01@protonmail.com>
2024-03-16 16:20:37 -0700

    Update to major Prime 95 source release

--- gwnum_linux.sh
+++ gwnum_linux.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-P95_URL=https://mersenne.org/download/software/v30/30.19/p95v3019b12.source.zip
-echo "Loading P95 version 30.19 build 12"
+P95_URL="https://www.mersenne.org/download/software/v30/30.19/p95v3019b13.source.zip"
+echo "Loading P95 version 30.19 build 13"
 [ -d "/tmp/P95/" ] && rm -r /tmp/P95
 mkdir /tmp/P95
 wget -q -O /tmp/P95/P95_source.zip $P95_URL
